769ec2b3329c4efbfc1165e8ff1cb5b3cfca0c9b
Circle CI <circleci@agentvi.com>
2020-03-09 14:25:07 +0000

    updated lib

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,12 +1,12 @@
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { SensorDebugInfo } from '../entities/SensorDebugInfo';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorDebugInfo } from '../entities/SensorDebugInfo';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { BaseEntity } from '../entities/BaseEntity';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
