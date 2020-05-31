e330e82b0bba6de8c858f727f0e21171cde95bdb
Circle CI <circleci@agentvi.com>
2020-05-31 14:31:52 +0000

    updated lib

--- lib/common/HealthThresholds.d.ts
+++ lib/common/HealthThresholds.d.ts
@@ -1,6 +1,7 @@
 import { Thresholds } from '../common/Thresholds';
 export declare class HealthThresholds {
+    isEmpty: boolean;
     sensorsThresholds: Thresholds;
     applianceThresholds: Thresholds;
-    constructor(sensorsThresholds?: Thresholds, applianceThresholds?: Thresholds);
+    constructor(isEmpty?: boolean, sensorsThresholds?: Thresholds, applianceThresholds?: Thresholds);
 }
--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
+import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Thresholds } from '../common/Thresholds';
-import { BaseEntity } from '../entities/BaseEntity';
+import { SensorDebugInfo } from '../common/SensorDebugInfo';
+import { StringKeyValue } from '../common/StringKeyValue';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
