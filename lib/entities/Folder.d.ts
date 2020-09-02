51934fc31a90cc01307ed08d05ad630845863658
Circle CI <circleci@agentvi.com>
2020-09-02 11:19:33 +0000

    updated lib

--- lib/entities/Folder.d.ts
+++ lib/entities/Folder.d.ts
@@ -1,6 +1,7 @@
-import { HealthThresholds } from '../common/HealthThresholds';
-import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
+import { HealthThresholds } from '../common/HealthThresholds';
+import { TimeFrame } from '../common/TimeFrame';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class Folder extends BaseEntity {
     parentId: string;
     accountId: string;
@@ -12,6 +13,7 @@ export declare class Folder extends BaseEntity {
     geoLocation: Coordinate;
     timezone: string;
     healthThresholds: HealthThresholds;
+    shunt: TimeFrame;
     id: string;
     _type: string;
     createdOn: number;
