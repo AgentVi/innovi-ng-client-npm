4fceca364187f7f84b54f2a11c2469702cb6ec59
Circle CI <circleci@agentvi.com>
2020-09-24 10:44:31 +0000

    updated lib

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StringKeyValue } from '../common/StringKeyValue';
+import { Thresholds } from '../common/Thresholds';
+import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { BaseEntity } from '../entities/BaseEntity';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { Coordinate } from '../common/Coordinate';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { Thresholds } from '../common/Thresholds';
-import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
@@ -35,6 +35,7 @@ export declare class Sensor extends BaseEntity {
     path: string;
     healthThresholds: Thresholds;
     debugInfo: SensorDebugInfo;
+    syncTimeToStream: boolean;
     id: string;
     _type: string;
     createdOn: number;
