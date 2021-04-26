87494c823ec40a94445b2beb1f7b0810a057bdfb
Circle CI <circleci@agentvi.com>
2021-04-26 10:57:42 +0000

    updated lib

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
 import { StringKeyValue } from '../common/StringKeyValue';
-import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { Thresholds } from '../common/Thresholds';
 import { BaseEntity } from '../entities/BaseEntity';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorDebugInfo } from '../common/SensorDebugInfo';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
--- lib/enums/UserTypeCode.d.ts
+++ lib/enums/UserTypeCode.d.ts
@@ -2,7 +2,6 @@ export declare enum UserTypeCode {
     UNDEFINED = 0,
     SYSADMIN = 1,
     SUPPORT = 2,
-    PRESALE = 3,
     ACCOUNT_USER = 4,
     ACCOUNT_SERVICE = 5
 }
