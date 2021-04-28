a97f8ae3a6a1c3fbcac061c62102a854194c2e25
Circle CI <circleci@agentvi.com>
2021-04-28 09:11:41 +0000

    updated lib

--- lib/entities/Account.d.ts
+++ lib/entities/Account.d.ts
@@ -1,9 +1,9 @@
-import { AccountTypeCode } from '../enums/AccountTypeCode';
-import { AccountStatusCode } from '../enums/AccountStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { AccountSettings } from '../common/AccountSettings';
 import { HealthThresholds } from '../common/HealthThresholds';
 import { BaseEntity } from '../entities/BaseEntity';
+import { AccountTypeCode } from '../enums/AccountTypeCode';
+import { AccountStatusCode } from '../enums/AccountStatusCode';
 export declare class Account extends BaseEntity {
     name: string;
     key: string;
@@ -19,6 +19,7 @@ export declare class Account extends BaseEntity {
     description: string;
     settings: AccountSettings;
     healthThresholds: HealthThresholds;
+    sysAdminSupport: boolean;
     id: string;
     _type: string;
     createdOn: number;
--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
-import { StringKeyValue } from '../common/StringKeyValue';
-import { Thresholds } from '../common/Thresholds';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
+import { Coordinate } from '../common/Coordinate';
+import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { BaseEntity } from '../entities/BaseEntity';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StringKeyValue } from '../common/StringKeyValue';
+import { Thresholds } from '../common/Thresholds';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Coordinate } from '../common/Coordinate';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { SensorDebugInfo } from '../common/SensorDebugInfo';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
