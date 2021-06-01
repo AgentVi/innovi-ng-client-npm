87b3799a6593642b092c7d78125f88d8db9031f0
Circle CI <circleci@agentvi.com>
2021-06-01 07:37:49 +0000

    updated lib

--- lib/common/MemberRegistration.d.ts
+++ lib/common/MemberRegistration.d.ts
@@ -1,14 +1,14 @@
-import { AccountRole } from '../common/AccountRole';
+import { AccountRoleCode } from '../enums/AccountRoleCode';
 export declare class MemberRegistration {
     accountId: string;
     name: string;
     email: string;
     mobile: string;
-    role: AccountRole;
+    role: AccountRoleCode;
     groups: string[];
     tempPassword: string;
     changePassword: boolean;
     verifyByEmail: boolean;
     description: string;
-    constructor(accountId?: string, name?: string, email?: string, mobile?: string, role?: AccountRole, groups?: string[], tempPassword?: string, changePassword?: boolean, verifyByEmail?: boolean, description?: string);
+    constructor(accountId?: string, name?: string, email?: string, mobile?: string, role?: AccountRoleCode, groups?: string[], tempPassword?: string, changePassword?: boolean, verifyByEmail?: boolean, description?: string);
 }
