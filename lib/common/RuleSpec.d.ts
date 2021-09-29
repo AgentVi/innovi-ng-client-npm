a180954dc1cfbc9d5135192e136af9187f81b5db
Circle CI <circleci@agentvi.com>
2021-09-29 12:45:16 +0000

    updated lib

--- lib/common/RuleSpec.d.ts
+++ lib/common/RuleSpec.d.ts
@@ -1,7 +1,8 @@
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { RuleDefault } from '../common/RuleDefault';
 import { ObjectTypeNode } from '../common/ObjectTypeNode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 export declare class RuleSpec {
     behaviorType: BehaviorTypeCode;
     ruleTypeName: string;
@@ -14,5 +15,6 @@ export declare class RuleSpec {
     intervalTime: RuleDefault;
     referenceCrop: boolean;
     objectHierarchy: ObjectTypeNode[];
-    constructor(behaviorType?: BehaviorTypeCode, ruleTypeName?: string, objectTypes?: ObjectTypeCode[], isLineDrawing?: boolean, dwellTime?: RuleDefault, minSpeed?: RuleDefault, peopleInGroup?: RuleDefault, clusterDistance?: RuleDefault, intervalTime?: RuleDefault, referenceCrop?: boolean, objectHierarchy?: ObjectTypeNode[]);
+    sensorTypes: SensorTypeCode[];
+    constructor(behaviorType?: BehaviorTypeCode, ruleTypeName?: string, objectTypes?: ObjectTypeCode[], isLineDrawing?: boolean, dwellTime?: RuleDefault, minSpeed?: RuleDefault, peopleInGroup?: RuleDefault, clusterDistance?: RuleDefault, intervalTime?: RuleDefault, referenceCrop?: boolean, objectHierarchy?: ObjectTypeNode[], sensorTypes?: SensorTypeCode[]);
 }
