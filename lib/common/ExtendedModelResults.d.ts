7b200b53cf0e4056ff274c175a276358e92fc6a9
Circle CI <circleci@agentvi.com>
2021-12-05 09:24:06 +0000

    Update lib: 2.1.626

--- lib/common/ExtendedModelResults.d.ts
+++ lib/common/ExtendedModelResults.d.ts
@@ -1,6 +1,6 @@
+import { Attribute } from '../common/Attribute';
 import { BoundingBox } from '../common/BoundingBox';
 import { Embedding } from '../common/Embedding';
-import { Attribute } from '../common/Attribute';
 export declare class ExtendedModelResults {
     modelName: string;
     attributes: Attribute[];
--- lib/entities/SearchEvent.d.ts
+++ lib/entities/SearchEvent.d.ts
@@ -1,11 +1,11 @@
-import { BoundingBox } from '../common/BoundingBox';
-import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
+import { BoundingBox } from '../common/BoundingBox';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchEvent extends BaseEntity {
     sensorId: string;
     extSensorId: string;
