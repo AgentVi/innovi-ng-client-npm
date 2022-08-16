84f39aa6bc01f0e958296c07cd0abddcede697a1
Circle CI <circleci@agentvi.com>
2022-08-16 12:28:47 +0000

    Update lib: 2.1.746

--- lib/entities/Account.d.ts
+++ lib/entities/Account.d.ts
@@ -1,10 +1,10 @@
+import { BaseEntity } from '../entities/BaseEntity';
 import { AccountTypeCode } from '../enums/AccountTypeCode';
 import { AccountStatusCode } from '../enums/AccountStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { AccountSettings } from '../common/AccountSettings';
 import { HealthThresholds } from '../common/HealthThresholds';
 import { PlatformTypeCode } from '../enums/PlatformTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
 export declare class Account extends BaseEntity {
     name: string;
     key: string;
@@ -22,6 +22,7 @@ export declare class Account extends BaseEntity {
     healthThresholds: HealthThresholds;
     sysAdminSupport: boolean;
     platformType: PlatformTypeCode;
+    eventClipLengthSec: number;
     id: string;
     _type: string;
     createdOn: number;
--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,14 +1,14 @@
-import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { StringKeyValue } from '../common/StringKeyValue';
+import { Thresholds } from '../common/Thresholds';
+import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { BaseEntity } from '../entities/BaseEntity';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { Coordinate } from '../common/Coordinate';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Coordinate } from '../common/Coordinate';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
@@ -45,6 +45,7 @@ export declare class Sensor extends BaseEntity {
     viewType: ViewTypeCode;
     poseEstimation: boolean;
     eventFiltering: boolean;
+    eventClipLengthSec: number;
     id: string;
     _type: string;
     createdOn: number;
