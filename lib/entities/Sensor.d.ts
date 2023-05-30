5b50845cf59f52d4692c59332baec0154ee1df2d
Circle CI <circleci@agentvi.com>
2023-05-30 08:33:29 +0000

    Update lib: 2.2.upgrade-go-version-mha-1211

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,14 +1,14 @@
 import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
