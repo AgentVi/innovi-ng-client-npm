49366f1b9556972b8002e4f5fe7dc0ed79d0d5bb
Circle CI <circleci@agentvi.com>
2023-06-06 11:51:12 +0000

    Update lib: 2.2.1236

--- lib/common/RuleSpec.d.ts
+++ lib/common/RuleSpec.d.ts
@@ -1,4 +1,3 @@
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { RuleDefault } from '../common/RuleDefault';
 import { ObjectTypeNode } from '../common/ObjectTypeNode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
@@ -6,6 +5,7 @@ import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { RuleAutoSensitivityCode } from '../enums/RuleAutoSensitivityCode';
 import { RuleDefaultSensitivity } from '../common/RuleDefaultSensitivity';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 export declare class RuleSpec {
     behaviorType: BehaviorTypeCode;
     ruleTypeName: string;
