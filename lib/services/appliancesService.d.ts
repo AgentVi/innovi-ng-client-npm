4c3090819378d8966fd731a062e93fcfe03ba811
Circle CI <circleci@agentvi.com>
2023-08-18 13:15:31 +0000

    Update lib: 2.2.feat/pipeline-integration-1697

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,15 +1,15 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { Sensor } from '../entities/Sensor';
-import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Sensor } from '../entities/Sensor';
 import { ApplianceRegistration } from '../common/ApplianceRegistration';
 import { ProductTypeCode } from '../enums/ProductTypeCode';
-import { Appliance } from '../entities/Appliance';
-import { CommandStatusCode } from '../enums/CommandStatusCode';
 import { AgentStatusCode } from '../enums/AgentStatusCode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { CommandStatusCode } from '../enums/CommandStatusCode';
+import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
+import { Appliance } from '../entities/Appliance';
 import * as i0 from "@angular/core";
 /**
  * List of appliance related actions
