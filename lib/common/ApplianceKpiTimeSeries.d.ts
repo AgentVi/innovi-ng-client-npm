1afd59de14e0e687e08cdc382ce9b1ffd00a68db
Circle CI <circleci@agentvi.com>
2023-08-21 10:49:34 +0000

    Update lib: 2.2.1709

--- lib/common/ApplianceKpiTimeSeries.d.ts
+++ lib/common/ApplianceKpiTimeSeries.d.ts
@@ -1,5 +1,5 @@
-import { TimeUnitCode } from '../enums/TimeUnitCode';
 import { ApplianceKpiTimestamped } from '../common/ApplianceKpiTimestamped';
+import { TimeUnitCode } from '../enums/TimeUnitCode';
 export declare class ApplianceKpiTimeSeries {
     label: string;
     interval: TimeUnitCode;
