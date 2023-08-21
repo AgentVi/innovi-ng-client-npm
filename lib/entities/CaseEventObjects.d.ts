afb8a119cdf7ce117a704d527660bb2eb1ae6662
Circle CI <circleci@agentvi.com>
2023-08-21 10:12:08 +0000

    Update lib: 2.2.1707

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,11 +1,11 @@
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
-import { TimestampedCrop } from '../common/TimestampedCrop';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
+import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,14 +1,14 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Preset } from '../entities/Preset';
-import { Sensor } from '../entities/Sensor';
-import { Coordinate } from '../common/Coordinate';
 import { RulePolygon } from '../common/RulePolygon';
+import { Sensor } from '../entities/Sensor';
 import { SensorsGroup } from '../common/SensorsGroup';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { Preset } from '../entities/Preset';
+import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
