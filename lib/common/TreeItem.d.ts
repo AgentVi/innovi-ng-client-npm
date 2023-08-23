984f7aded70caed9b3b9ad25094282e103e02a8e
Circle CI <circleci@agentvi.com>
2023-08-23 14:23:36 +0000

    Update lib: 2.2.1726

--- lib/common/TreeItem.d.ts
+++ lib/common/TreeItem.d.ts
@@ -2,6 +2,7 @@ import { EntityTypeCode } from '../enums/EntityTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { TimeFrame } from '../common/TimeFrame';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { PermissionMask } from '../enums/PermissionMask';
 export declare class TreeItem {
     itemId: string;
     type: EntityTypeCode;
@@ -12,5 +13,6 @@ export declare class TreeItem {
     scheduleId: string;
     active: boolean;
     behaviorType: BehaviorTypeCode;
-    constructor(itemId?: string, type?: EntityTypeCode, name?: string, typeName?: string, status?: SensorStatusCode, shunt?: TimeFrame, scheduleId?: string, active?: boolean, behaviorType?: BehaviorTypeCode);
+    permissions: PermissionMask;
+    constructor(itemId?: string, type?: EntityTypeCode, name?: string, typeName?: string, status?: SensorStatusCode, shunt?: TimeFrame, scheduleId?: string, active?: boolean, behaviorType?: BehaviorTypeCode, permissions?: PermissionMask);
 }
