bbb204d695318313b67e83a6ad2789084dd60df4
Circle CI <circleci@agentvi.com>
2023-10-10 11:54:41 +0000

    Update lib: 2.2.fix/inv-6379-report-events-after-last-2134

--- lib/messages/ReportsServiceUpdateRequest.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReportDefinition } from '../entities/ReportDefinition';
-export declare class ReportsServiceUpdateRequest {
-    id: string;
-    body: ReportDefinition;
-    constructor(id?: string, body?: ReportDefinition);
-}
