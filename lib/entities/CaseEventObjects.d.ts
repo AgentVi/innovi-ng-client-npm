2398945fb668fcae482a2727c8c66741842b58c1
Circle CI <circleci@agentvi.com>
2023-10-31 06:22:57 +0000

    Update lib: 2.2.feature/inv-6554-crowd-density-and-crowd-statistics-2251

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,11 +1,11 @@
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BaseEntity } from '../entities/BaseEntity';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
-import { Coordinate } from '../common/Coordinate';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { BoundingBox } from '../common/BoundingBox';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
--- lib/messages/EntityResponseOfReportDefinition.d.ts
+++ lib/messages/EntityResponseOfReportDefinition.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { ReportDefinition } from '../entities/ReportDefinition';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfReportDefinition extends EntityResponse {
     entity: ReportDefinition;
     code: number;
