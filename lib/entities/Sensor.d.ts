9e831b5a12d26d668685e3f664e3043a2d865688
Circle CI <circleci@agentvi.com>
2023-11-06 13:53:32 +0000

    Update lib: 2.2.fix/inv-5469-search-cross-tenant-2-2279

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { BaseEntity } from '../entities/BaseEntity';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { Coordinate } from '../common/Coordinate';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { StringKeyValue } from '../common/StringKeyValue';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Coordinate } from '../common/Coordinate';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
--- lib/messages/EntitiesResponseOfAccountTypeSummary.d.ts
+++ lib/messages/EntitiesResponseOfAccountTypeSummary.d.ts
@@ -1,5 +1,5 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
 import { AccountTypeSummary } from '../common/AccountTypeSummary';
+import { EntitiesResponse } from '../messages/EntitiesResponse';
 export declare class EntitiesResponseOfAccountTypeSummary extends EntitiesResponse {
     list: AccountTypeSummary[];
     code: number;
