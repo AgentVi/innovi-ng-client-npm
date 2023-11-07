67d1f9759d015b8768c209711972821dddb69228
Circle CI <circleci@agentvi.com>
2023-11-07 05:58:04 +0000

    Update lib: 2.2.fix/inv-7725-unit-test-for-wrong-total-2291

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,15 +1,15 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Sensor } from '../entities/Sensor';
 import { Appliance } from '../entities/Appliance';
+import { ApplianceRegistration } from '../common/ApplianceRegistration';
+import { ProductTypeCode } from '../enums/ProductTypeCode';
 import { CommandStatusCode } from '../enums/CommandStatusCode';
 import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
 import { AgentStatusCode } from '../enums/AgentStatusCode';
-import { ApplianceRegistration } from '../common/ApplianceRegistration';
-import { ProductTypeCode } from '../enums/ProductTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { Sensor } from '../entities/Sensor';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import * as i0 from "@angular/core";
 /**
  * List of appliance related actions
