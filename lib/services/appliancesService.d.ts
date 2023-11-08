31b771aa095736ef1de8ac16ad8f908c78b6eb12
Circle CI <circleci@agentvi.com>
2023-11-08 11:43:41 +0000

    Update lib: 2.2.fix/remove-unused-token-ttl-env-variable-2310

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,15 +1,15 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
+import { AgentStatusCode } from '../enums/AgentStatusCode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Sensor } from '../entities/Sensor';
 import { ApplianceRegistration } from '../common/ApplianceRegistration';
-import { ProductTypeCode } from '../enums/ProductTypeCode';
 import { Appliance } from '../entities/Appliance';
 import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
-import { AgentStatusCode } from '../enums/AgentStatusCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { CommandStatusCode } from '../enums/CommandStatusCode';
+import { ProductTypeCode } from '../enums/ProductTypeCode';
 import * as i0 from "@angular/core";
 /**
  * List of appliance related actions
