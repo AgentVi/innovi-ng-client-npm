c3f5be2f04d0f7cd7596cf38aee6c2215b35d599
Circle CI <circleci@agentvi.com>
2023-11-08 09:51:58 +0000

    Update lib: 2.2.fix/inv-5469-search-cross-tenant-3-2308

--- lib/common/SensorInfo.d.ts
+++ lib/common/SensorInfo.d.ts
@@ -1,4 +1,3 @@
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
@@ -6,6 +5,7 @@ import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 export declare class SensorInfo {
     name: string;
     type: SensorTypeCode;
