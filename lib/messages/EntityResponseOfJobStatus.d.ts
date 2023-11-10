037a56ac4b9b8f839bd7b55e5f1b615c7b128638
Circle CI <circleci@agentvi.com>
2023-11-10 15:54:19 +0000

    Update lib: 2.2.fix/inv-5469-search-cross-tenant-2-2328

--- lib/messages/EntityResponseOfJobStatus.d.ts
+++ lib/messages/EntityResponseOfJobStatus.d.ts
@@ -1,5 +1,5 @@
-import { JobStatus } from '../entities/JobStatus';
 import { EntityResponse } from '../messages/EntityResponse';
+import { JobStatus } from '../entities/JobStatus';
 export declare class EntityResponseOfJobStatus extends EntityResponse {
     entity: JobStatus;
     code: number;
