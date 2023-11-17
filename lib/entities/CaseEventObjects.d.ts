9fb2dc58ac6a5ed65a58ac4a26d20b69efa0a30c
Circle CI <circleci@agentvi.com>
2023-11-17 16:59:08 +0000

    Update lib: 2.2.feature/inv-5956-add-env-variable-to-import-initial-data-2377

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,11 +1,11 @@
-import { TimestampedCrop } from '../common/TimestampedCrop';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
 import { BoundingBox } from '../common/BoundingBox';
+import { TimestampedCrop } from '../common/TimestampedCrop';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { Coordinate } from '../common/Coordinate';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
--- lib/entities/Configuration.d.ts
+++ lib/entities/Configuration.d.ts
@@ -1,5 +1,5 @@
-import { BaseEntity } from '../entities/BaseEntity';
 import { ProductTypeCode } from '../enums/ProductTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class Configuration extends BaseEntity {
     description: string;
     currentVersion: string;
