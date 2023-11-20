927a1bf312f6e4f6fa37ab8f9b565f5d7759e055
Circle CI <circleci@agentvi.com>
2023-11-20 10:04:02 +0000

    Update lib: 2.2.feature/add-error-not-found-2383

--- lib/messages/EntityResponseOfGeoReferenceData.d.ts
+++ lib/messages/EntityResponseOfGeoReferenceData.d.ts
@@ -1,5 +1,5 @@
-import { GeoReferenceData } from '../common/GeoReferenceData';
 import { EntityResponse } from '../messages/EntityResponse';
+import { GeoReferenceData } from '../common/GeoReferenceData';
 export declare class EntityResponseOfGeoReferenceData extends EntityResponse {
     entity: GeoReferenceData;
     code: number;
