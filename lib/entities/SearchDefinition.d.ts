adbecb12e96aacb08e6b5e667798de7ee8f1048e
Circle CI <circleci@agentvi.com>
2023-11-27 10:36:59 +0000

    Update lib: 2.2.feature/inv-7924-new-stopped-object-logic-2443

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { TimeFrame } from '../common/TimeFrame';
-import { SearchObject } from '../common/SearchObject';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { Coordinate } from '../common/Coordinate';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchObject } from '../common/SearchObject';
+import { BaseEntity } from '../entities/BaseEntity';
+import { Coordinate } from '../common/Coordinate';
 import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
-import { BaseEntity } from '../entities/BaseEntity';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { TimeFrame } from '../common/TimeFrame';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
