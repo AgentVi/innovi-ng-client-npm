629920f2767127e017892c8a906b835a930e66ed
Circle CI <circleci@agentvi.com>
2023-11-28 14:29:42 +0000

    Update lib: 2.2.fix/inv-5469-search-cross-tenant-3-2453

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,14 +1,14 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { SensorsGroup } from '../common/SensorsGroup';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Preset } from '../entities/Preset';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { Coordinate } from '../common/Coordinate';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Preset } from '../entities/Preset';
+import { SensorsGroup } from '../common/SensorsGroup';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
