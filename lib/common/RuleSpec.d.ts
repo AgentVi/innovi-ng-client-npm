21d1a58f094a08c690d8eef140f0e6ea345ab590
Circle CI <circleci@agentvi.com>
2023-11-28 16:02:56 +0000

    Update lib: 2.2.fix/report-writer-refactor-2456

--- lib/common/RuleSpec.d.ts
+++ lib/common/RuleSpec.d.ts
@@ -1,3 +1,4 @@
+import { RuleDefault } from '../common/RuleDefault';
 import { ObjectTypeNode } from '../common/ObjectTypeNode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
@@ -5,7 +6,6 @@ import { RuleAutoSensitivityCode } from '../enums/RuleAutoSensitivityCode';
 import { RuleDefaultSensitivity } from '../common/RuleDefaultSensitivity';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { RuleDefault } from '../common/RuleDefault';
 export declare class RuleSpec {
     behaviorType: BehaviorTypeCode;
     ruleTypeName: string;
