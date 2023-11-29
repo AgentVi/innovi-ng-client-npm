2af50c20f0af2d82da78d3f5e86f8b6eb36872a0
Circle CI <circleci@agentvi.com>
2023-11-29 13:28:32 +0000

    Update lib: 2.2.feature/inv-7986-report-interval-2463

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,11 +1,11 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
+import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
 import { AgentStatusCode } from '../enums/AgentStatusCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { CommandStatusCode } from '../enums/CommandStatusCode';
-import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { CommandStatusCode } from '../enums/CommandStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Sensor } from '../entities/Sensor';
 import { ApplianceRegistration } from '../common/ApplianceRegistration';
 import { ProductTypeCode } from '../enums/ProductTypeCode';
