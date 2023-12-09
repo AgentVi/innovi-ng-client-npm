1c4ecc782b2bc6249a77c174c25ecf222394a8b2
Circle CI <circleci@agentvi.com>
2023-12-09 10:49:20 +0000

    Update lib: 2.2.feature/inv-5956-import-data-on-startup-2535

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
 import { TimeFrame } from '../common/TimeFrame';
-import { BaseEntity } from '../entities/BaseEntity';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { Coordinate } from '../common/Coordinate';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchObject } from '../common/SearchObject';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -3,11 +3,11 @@ import { CoreConfig } from '../../config';
 import { Sensor } from '../entities/Sensor';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Preset } from '../entities/Preset';
 import { SensorsGroup } from '../common/SensorsGroup';
 import { Coordinate } from '../common/Coordinate';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { Preset } from '../entities/Preset';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
 /**
