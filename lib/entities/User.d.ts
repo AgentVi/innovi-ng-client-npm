1408284ecc30fcd9d9cf3acee2576ffd8143e96d
Circle CI <circleci@agentvi.com>
2023-12-12 11:20:47 +0000

    Update lib: 2.2.feature/INV-8301-2557

--- lib/entities/User.d.ts
+++ lib/entities/User.d.ts
@@ -1,6 +1,6 @@
-import { BaseEntity } from '../entities/BaseEntity';
 import { UserTypeCode } from '../enums/UserTypeCode';
 import { UserStatusCode } from '../enums/UserStatusCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class User extends BaseEntity {
     name: string;
     email: string;
