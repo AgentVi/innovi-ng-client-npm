1ee1a49c25e74cb6f974fb72cea85a5251aa00fe
Circle CI <circleci@agentvi.com>
2023-12-13 14:30:59 +0000

    Update lib: 2.2.fix/inv-8266-process-redis-messages-in-fifo-order-2582

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,11 +1,11 @@
 import { BoundingBox } from '../common/BoundingBox';
+import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BaseEntity } from '../entities/BaseEntity';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { TimestampedCrop } from '../common/TimestampedCrop';
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { Coordinate } from '../common/Coordinate';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
