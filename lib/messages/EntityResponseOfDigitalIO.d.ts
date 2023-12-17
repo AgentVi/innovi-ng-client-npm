40a1bd93654184e11c086d21977f0445f693d2ff
Circle CI <circleci@agentvi.com>
2023-12-17 15:14:43 +0000

    Update lib: 2.2.fix/inv-8314-no-new-events-in-monitor-2611

--- lib/messages/EntityResponseOfDigitalIO.d.ts
+++ lib/messages/EntityResponseOfDigitalIO.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { DigitalIO } from '../entities/DigitalIO';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfDigitalIO extends EntityResponse {
     entity: DigitalIO;
     code: number;
