d8ce526a3576f2fc0d7e9fd30d844b50a76a8845
Circle CI <circleci@agentvi.com>
2023-12-20 14:19:49 +0000

    Update lib: 2.2.fix/inv-8266-process-redis-messages-in-fifo-order-2633

--- lib/entities/FeaturesGroup.d.ts
+++ lib/entities/FeaturesGroup.d.ts
@@ -1,5 +1,5 @@
-import { FeatureCode } from '../enums/FeatureCode';
 import { BaseEntity } from '../entities/BaseEntity';
+import { FeatureCode } from '../enums/FeatureCode';
 export declare class FeaturesGroup extends BaseEntity {
     name: string;
     features: FeatureCode[];
