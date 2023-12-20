cf4c109a0815edac6c49af821eea9aa4f825cf65
Circle CI <circleci@agentvi.com>
2023-12-20 15:13:04 +0000

    Update lib: 2.2.feature/INV-8189-2636

--- lib/entities/ScheduledReport.d.ts
+++ lib/entities/ScheduledReport.d.ts
@@ -1,8 +1,8 @@
+import { ReportTypeCode } from '../enums/ReportTypeCode';
 import { ReportFormatCode } from '../enums/ReportFormatCode';
 import { ReportDataTypeCode } from '../enums/ReportDataTypeCode';
 import { TimeUnitCode } from '../enums/TimeUnitCode';
 import { BaseEntity } from '../entities/BaseEntity';
-import { ReportTypeCode } from '../enums/ReportTypeCode';
 export declare class ScheduledReport extends BaseEntity {
     accountId: string;
     reportId: string;
