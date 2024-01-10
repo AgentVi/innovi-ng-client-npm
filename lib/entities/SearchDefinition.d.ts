9fc47092dcbee3f6716d0819ff6e92a0af886913
Circle CI <circleci@agentvi.com>
2024-01-10 13:51:21 +0000

    Update lib: 2.2.fix/inv-8413-reports-crowd-density-legacy-rule-type-2695

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { Coordinate } from '../common/Coordinate';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { SearchBehavior } from '../common/SearchBehavior';
-import { SearchObject } from '../common/SearchObject';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { TimeFrame } from '../common/TimeFrame';
 import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchObject } from '../common/SearchObject';
+import { BaseEntity } from '../entities/BaseEntity';
+import { Coordinate } from '../common/Coordinate';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { SearchBehavior } from '../common/SearchBehavior';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
