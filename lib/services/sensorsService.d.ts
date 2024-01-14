0f55f73476a25d6e5ff1f9b38a973661879ddffb
Circle CI <circleci@agentvi.com>
2024-01-14 17:53:45 +0000

    Update lib: 2.2.feature/INV-8419-unexpected-EOF-logging-2723

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -3,12 +3,12 @@ import { CoreConfig } from '../../config';
 import { RulePolygon } from '../common/RulePolygon';
 import { SensorsGroup } from '../common/SensorsGroup';
 import { Coordinate } from '../common/Coordinate';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Sensor } from '../entities/Sensor';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
+import { Sensor } from '../entities/Sensor';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
