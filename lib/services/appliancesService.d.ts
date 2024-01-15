fdbc81d332b6da7faf0461096005d512fb25db06
Circle CI <circleci@agentvi.com>
2024-01-15 06:05:58 +0000

    Update lib: 2.2.feature/inv-8359-create-analytics-config-service-2724

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -3,13 +3,13 @@ import { CoreConfig } from '../../config';
 import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
 import { Sensor } from '../entities/Sensor';
 import { ApplianceRegistration } from '../common/ApplianceRegistration';
-import { CommandStatusCode } from '../enums/CommandStatusCode';
+import { ProductTypeCode } from '../enums/ProductTypeCode';
 import { Appliance } from '../entities/Appliance';
 import { AgentStatusCode } from '../enums/AgentStatusCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { ProductTypeCode } from '../enums/ProductTypeCode';
+import { CommandStatusCode } from '../enums/CommandStatusCode';
 import * as i0 from "@angular/core";
 /**
  * List of appliance related actions
