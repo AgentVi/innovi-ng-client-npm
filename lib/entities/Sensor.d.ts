126a1db86dd6fc33d3bbc7abc4087e1f98f1cb73
Circle CI <circleci@agentvi.com>
2024-01-16 13:10:15 +0000

    Update lib: 2.2.feature/8166-additional-anon-types-2-2746

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
-import { Coordinate } from '../common/Coordinate';
-import { Thresholds } from '../common/Thresholds';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StringKeyValue } from '../common/StringKeyValue';
-import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
+import { Thresholds } from '../common/Thresholds';
 import { BaseEntity } from '../entities/BaseEntity';
+import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
+import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorDebugInfo } from '../common/SensorDebugInfo';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
--- lib/messages/EntityResponseOfCalendar.d.ts
+++ lib/messages/EntityResponseOfCalendar.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { Calendar } from '../entities/Calendar';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfCalendar extends EntityResponse {
     entity: Calendar;
     code: number;
