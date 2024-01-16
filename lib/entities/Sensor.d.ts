54cb1cca7a57e01e141e6ae0b0b1ab95ad1bd240
Circle CI <circleci@agentvi.com>
2024-01-16 11:32:52 +0000

    Update lib: 2.2.fix/inv-8266-process-redis-messages-in-fifo-order-2743

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { BaseEntity } from '../entities/BaseEntity';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Coordinate } from '../common/Coordinate';
-import { StringKeyValue } from '../common/StringKeyValue';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
+import { StringKeyValue } from '../common/StringKeyValue';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
