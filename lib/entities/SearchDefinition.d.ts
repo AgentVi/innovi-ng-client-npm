16e2173c0a73f6a634b703df363f181afe0eab32
Circle CI <circleci@agentvi.com>
2024-01-23 14:42:04 +0000

    Update lib: 2.2.feature/8166-additional-anon-types-2-2759

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,10 +1,10 @@
-import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { TimeFrame } from '../common/TimeFrame';
 import { SearchObject } from '../common/SearchObject';
-import { BaseEntity } from '../entities/BaseEntity';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { TimeFrame } from '../common/TimeFrame';
 import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
 export declare class SearchDefinition extends BaseEntity {
