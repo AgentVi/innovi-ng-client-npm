8c6532bed87412bdda78b41338fce85da2a1ec1b
Circle CI <circleci@agentvi.com>
2024-01-25 09:37:59 +0000

    Update lib: 2.2.feature/INV-8445-scan-model-repository-2772

--- lib/messages/EntityResponseOfUserAccountInfo.d.ts
+++ lib/messages/EntityResponseOfUserAccountInfo.d.ts
@@ -1,5 +1,5 @@
-import { UserAccountInfo } from '../common/UserAccountInfo';
 import { EntityResponse } from '../messages/EntityResponse';
+import { UserAccountInfo } from '../common/UserAccountInfo';
 export declare class EntityResponseOfUserAccountInfo extends EntityResponse {
     entity: UserAccountInfo;
     code: number;
