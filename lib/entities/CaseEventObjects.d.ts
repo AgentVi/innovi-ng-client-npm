07bcdc65628e232be9aac29db7c7e7cbf19b0cca
Circle CI <circleci@agentvi.com>
2024-01-26 13:33:14 +0000

    Update lib: 2.2.fix/inv-8251-monitor-event-images-2783

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,8 +1,8 @@
-import { BaseEntity } from '../entities/BaseEntity';
 import { EventStatusCode } from '../enums/EventStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { BoundingBox } from '../common/BoundingBox';
 import { TimestampedCrop } from '../common/TimestampedCrop';
+import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
--- lib/messages/EntitiesResponseOfIntegrationAction.d.ts
+++ lib/messages/EntitiesResponseOfIntegrationAction.d.ts
@@ -1,5 +1,5 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
 import { IntegrationAction } from '../entities/IntegrationAction';
+import { EntitiesResponse } from '../messages/EntitiesResponse';
 export declare class EntitiesResponseOfIntegrationAction extends EntitiesResponse {
     list: IntegrationAction[];
     code: number;
