37b51904e40a4fde78aeddc8f7828b261f723a53
Circle CI <circleci@agentvi.com>
2024-01-29 08:31:06 +0000

    Update lib: 2.2.feature/inv-5468-scheduled-report-cross-tenant-2796

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,14 +1,14 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { Sensor } from '../entities/Sensor';
-import { SensorsGroup } from '../common/SensorsGroup';
-import { Coordinate } from '../common/Coordinate';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { RulePolygon } from '../common/RulePolygon';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
+import { Sensor } from '../entities/Sensor';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { RulePolygon } from '../common/RulePolygon';
+import { SensorsGroup } from '../common/SensorsGroup';
+import { Coordinate } from '../common/Coordinate';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
