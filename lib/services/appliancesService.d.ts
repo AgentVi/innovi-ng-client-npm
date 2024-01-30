cae61d4114b6609ddae68728f32ec292e96c00ba
Circle CI <circleci@agentvi.com>
2024-01-30 09:17:23 +0000

    Update lib: 2.2.feature/8166-additional-anon-types-2-2810

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,15 +1,15 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { ProductTypeCode } from '../enums/ProductTypeCode';
-import { Appliance } from '../entities/Appliance';
 import { CommandStatusCode } from '../enums/CommandStatusCode';
 import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { ApplianceRegistration } from '../common/ApplianceRegistration';
-import { Sensor } from '../entities/Sensor';
-import { AgentStatusCode } from '../enums/AgentStatusCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { Sensor } from '../entities/Sensor';
+import { Appliance } from '../entities/Appliance';
+import { ProductTypeCode } from '../enums/ProductTypeCode';
+import { AgentStatusCode } from '../enums/AgentStatusCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { ApplianceRegistration } from '../common/ApplianceRegistration';
 import * as i0 from "@angular/core";
 /**
  * List of appliance related actions
