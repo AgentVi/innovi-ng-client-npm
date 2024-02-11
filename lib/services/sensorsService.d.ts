f65e99454c85dc0a004e2e2547b427e5ec7d1c24
Circle CI <circleci@agentvi.com>
2024-02-11 13:20:13 +0000

    Update lib: 2.2.fix/inv-6192-multi-select-filter-2903

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,13 +1,13 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { Sensor } from '../entities/Sensor';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { RulePolygon } from '../common/RulePolygon';
+import { SensorsGroup } from '../common/SensorsGroup';
+import { Coordinate } from '../common/Coordinate';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
-import { SensorsGroup } from '../common/SensorsGroup';
-import { Coordinate } from '../common/Coordinate';
+import { Sensor } from '../entities/Sensor';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { RulePolygon } from '../common/RulePolygon';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import * as i0 from "@angular/core";
 /**
