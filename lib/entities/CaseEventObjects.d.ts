144effcc396c630e3d129dd2ae7c81a0c8127abb
Circle CI <circleci@agentvi.com>
2024-02-15 12:45:18 +0000

    Update lib: 2.2.feature/INV-6553-2911

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,9 +1,9 @@
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
-import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { Coordinate } from '../common/Coordinate';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
 import { TimestampedCrop } from '../common/TimestampedCrop';
