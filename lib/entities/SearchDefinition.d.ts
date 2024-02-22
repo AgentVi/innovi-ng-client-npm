fbae4255abce2a63ebaa97fe30d6d91a72c3ea6f
Circle CI <circleci@agentvi.com>
2024-02-22 03:26:31 +0000

    Update lib: 2.2.feature/inv-8274-merge-recovery-code-2955

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
 import { Coordinate } from '../common/Coordinate';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { TimeFrame } from '../common/TimeFrame';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { SearchObject } from '../common/SearchObject';
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
