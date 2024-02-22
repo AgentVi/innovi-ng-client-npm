b587067012435972452673e5a46a79ffb12249b4
Circle CI <circleci@agentvi.com>
2024-02-22 05:54:31 +0000

    Update lib: 2.2.feature/inv-8274-merge-recovery-code-2960

--- lib/common/RuleSpec.d.ts
+++ lib/common/RuleSpec.d.ts
@@ -1,3 +1,4 @@
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { RuleDefault } from '../common/RuleDefault';
 import { ObjectTypeNode } from '../common/ObjectTypeNode';
@@ -5,7 +6,6 @@ import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { RuleAutoSensitivityCode } from '../enums/RuleAutoSensitivityCode';
 import { RuleDefaultSensitivity } from '../common/RuleDefaultSensitivity';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 export declare class RuleSpec {
     behaviorType: BehaviorTypeCode;
     ruleTypeName: string;
--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,14 +1,14 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
+import { Appliance } from '../entities/Appliance';
 import { AgentStatusCode } from '../enums/AgentStatusCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Sensor } from '../entities/Sensor';
-import { ApplianceRegistration } from '../common/ApplianceRegistration';
-import { Appliance } from '../entities/Appliance';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { ProductTypeCode } from '../enums/ProductTypeCode';
+import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
+import { ApplianceRegistration } from '../common/ApplianceRegistration';
 import { CommandStatusCode } from '../enums/CommandStatusCode';
 import * as i0 from "@angular/core";
 /**
