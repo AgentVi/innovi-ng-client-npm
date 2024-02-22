6b6400bb592c699144e89ae7b6aa0ceaf0c70fdb
Circle CI <circleci@agentvi.com>
2024-02-22 17:37:41 +0000

    Update lib: 2.2.feature/INV-6553-2974

--- lib/messages/EntityResponseOfEvent.d.ts
+++ lib/messages/EntityResponseOfEvent.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { Event } from '../entities/Event';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfEvent extends EntityResponse {
     entity: Event;
     code: number;
