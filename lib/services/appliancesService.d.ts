1589db34bc5ee31aadaecfb110aac19df1a485bf
Circle CI <circleci@agentvi.com>
2024-02-28 15:18:09 +0000

    Update lib: 2.2.feature/per-camera-billing-3009

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,15 +1,15 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { ProductTypeCode } from '../enums/ProductTypeCode';
-import { Appliance } from '../entities/Appliance';
 import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
+import { AgentStatusCode } from '../enums/AgentStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { ApplianceRegistration } from '../common/ApplianceRegistration';
-import { Sensor } from '../entities/Sensor';
-import { CommandStatusCode } from '../enums/CommandStatusCode';
-import { AgentStatusCode } from '../enums/AgentStatusCode';
+import { ProductTypeCode } from '../enums/ProductTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { Sensor } from '../entities/Sensor';
+import { Appliance } from '../entities/Appliance';
+import { CommandStatusCode } from '../enums/CommandStatusCode';
 import * as i0 from "@angular/core";
 /**
  * List of appliance related actions
