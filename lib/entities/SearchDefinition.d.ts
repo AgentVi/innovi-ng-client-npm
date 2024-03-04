61c148688f8495a5e0cc9d3b42060c07ff244581
Circle CI <circleci@agentvi.com>
2024-03-04 09:21:00 +0000

    Update lib: 2.2.fix/build-images-unavailable-3037

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
-import { Coordinate } from '../common/Coordinate';
-import { SearchBehavior } from '../common/SearchBehavior';
-import { ObjectData } from '../common/ObjectData';
-import { SearchObject } from '../common/SearchObject';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchObject } from '../common/SearchObject';
+import { ObjectData } from '../common/ObjectData';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { BaseEntity } from '../entities/BaseEntity';
 import { TimeFrame } from '../common/TimeFrame';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
