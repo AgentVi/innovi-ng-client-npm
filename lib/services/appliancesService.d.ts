73bcde9ffaa65d61077f347324d7d1d8a6ea4b26
Circle CI <circleci@agentvi.com>
2024-03-14 12:41:30 +0000

    Update lib: 2.2.feature/inv-8622-add-ad-search-3079

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { AgentStatusCode } from '../enums/AgentStatusCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { ApplianceRegistration } from '../common/ApplianceRegistration';
-import { CommandStatusCode } from '../enums/CommandStatusCode';
 import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { ApplianceRegistration } from '../common/ApplianceRegistration';
+import { CommandStatusCode } from '../enums/CommandStatusCode';
+import { AgentStatusCode } from '../enums/AgentStatusCode';
 import { Sensor } from '../entities/Sensor';
 import { ProductTypeCode } from '../enums/ProductTypeCode';
 import { Appliance } from '../entities/Appliance';
