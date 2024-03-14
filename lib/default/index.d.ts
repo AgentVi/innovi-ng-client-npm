98bfa8ffe53cbd83eabff8a945a380bd05b3e77a
Circle CI <circleci@agentvi.com>
2024-03-14 15:01:31 +0000

    Update lib: 2.2.feature/inv-8622-add-ad-search-3086

--- lib/default/index.d.ts
+++ lib/default/index.d.ts
@@ -1,1 +1,2 @@
+export * from './DomainEntry';
 export * from './UpdateScheduleForRules';
---
+++ lib/messages/MembersServiceDomainSearchRequest.d.ts
@@ -0,0 +1,7 @@
+export declare class MembersServiceDomainSearchRequest {
+    search: string;
+    sort: string;
+    page: number;
+    pageSize: number;
+    constructor(search?: string, sort?: string, page?: number, pageSize?: number);
+}
