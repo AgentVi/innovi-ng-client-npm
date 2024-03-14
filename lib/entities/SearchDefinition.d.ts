608c09319c124df3abb30b2eeaa30ae948e33369
Circle CI <circleci@agentvi.com>
2024-03-14 17:08:07 +0000

    Update lib: 2.2.feature/inv-8326-8445-scan-model-update-rules-3088

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
+import { Coordinate } from '../common/Coordinate';
 import { TimeFrame } from '../common/TimeFrame';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
-import { Coordinate } from '../common/Coordinate';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
+import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { Coordinate } from '../common/Coordinate';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StringKeyValue } from '../common/StringKeyValue';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Coordinate } from '../common/Coordinate';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
