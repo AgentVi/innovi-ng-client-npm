d78500fbaac9e6f1254b317779552615bf618c36
Circle CI <circleci@agentvi.com>
2024-03-19 14:06:09 +0000

    Update lib: 2.2.feature/inv-8326-pai-create-update-rules-3111

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
-import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { ObjectInfo } from '../common/ObjectInfo';
 import { Coordinate } from '../common/Coordinate';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { BoundingBox } from '../common/BoundingBox';
 import { TimestampedCrop } from '../common/TimestampedCrop';
+import { BaseEntity } from '../entities/BaseEntity';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { ObjectInfo } from '../common/ObjectInfo';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
