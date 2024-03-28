f9b5bc4eef81dd151fdbaa290d0d5cd4ef293f6d
Circle CI <circleci@agentvi.com>
2024-03-28 14:49:20 +0000

    Update lib: 2.2.feature/inv-8326-pai-create-update-rules-3255

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,13 +1,13 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { RulePolygon } from '../common/RulePolygon';
-import { Sensor } from '../entities/Sensor';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Preset } from '../entities/Preset';
-import { SensorsGroup } from '../common/SensorsGroup';
+import { Sensor } from '../entities/Sensor';
 import { Coordinate } from '../common/Coordinate';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { RulePolygon } from '../common/RulePolygon';
+import { SensorsGroup } from '../common/SensorsGroup';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import * as i0 from "@angular/core";
 /**
