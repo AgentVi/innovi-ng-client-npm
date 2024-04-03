88f756a16e1d50a11ddd7bd8260114f0a5d04b54
Circle CI <circleci@agentvi.com>
2024-04-03 15:17:06 +0000

    Update lib: 2.2.feature/pai-demo-isc-west-3292

--- lib/common/RulePolygon.d.ts
+++ lib/common/RulePolygon.d.ts
@@ -1,6 +1,6 @@
+import { Point } from '../common/Point';
 import { RuleColorCode } from '../enums/RuleColorCode';
 import { LineCrossDirectionCode } from '../enums/LineCrossDirectionCode';
-import { Point } from '../common/Point';
 export declare class RulePolygon {
     lineCrossDir: LineCrossDirectionCode;
     points: Point[];
--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
-import { BoundingBox } from '../common/BoundingBox';
-import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BaseEntity } from '../entities/BaseEntity';
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { Coordinate } from '../common/Coordinate';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
+import { TimestampedCrop } from '../common/TimestampedCrop';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
+import { Coordinate } from '../common/Coordinate';
+import { BoundingBox } from '../common/BoundingBox';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
