d6f5d39ae24e10ed765e766d869bbd10516555cd
Circle CI <circleci@agentvi.com>
2024-04-05 11:51:28 +0000

    Update lib: 2.2.feature/inv-8716-ldap-login-using-usergroup-3317

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { Coordinate } from '../common/Coordinate';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { SearchObject } from '../common/SearchObject';
-import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
