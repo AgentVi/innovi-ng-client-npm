7ea1ae6fb557f71ed36ffe03b672c23d6bdaa1e6
Circle CI <circleci@agentvi.com>
2024-04-08 11:52:26 +0000

    Update lib: 2.2.feature/inv-8811-add-aduser-adusergroup-3355

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { Coordinate } from '../common/Coordinate';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { TimeFrame } from '../common/TimeFrame';
 import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchBehavior } from '../common/SearchBehavior';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { Coordinate } from '../common/Coordinate';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
