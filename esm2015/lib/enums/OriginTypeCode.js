d49a5897958919dfcb1c5354d0e9618a55881994
Circle CI <circleci@agentvi.com>
2024-04-11 09:14:16 +0000

    Update lib: 2.2.feature/inv-8811-add-aduser-adusergroup-3402

---
+++ esm2015/lib/enums/OriginTypeCode.js
@@ -0,0 +1,15 @@
+/*
+   Origin type code
+*/
+export var OriginTypeCode;
+(function (OriginTypeCode) {
+    // Undefined [0] 
+    OriginTypeCode[OriginTypeCode["UNDEFINED"] = 0] = "UNDEFINED";
+    // Registration by Email [1] 
+    OriginTypeCode[OriginTypeCode["EMAIL"] = 1] = "EMAIL";
+    // Registration by LDAP user [2] 
+    OriginTypeCode[OriginTypeCode["LDAP_USER"] = 2] = "LDAP_USER";
+    // Registration by LDAP user group [3] 
+    OriginTypeCode[OriginTypeCode["LDAP_GROUP"] = 3] = "LDAP_GROUP";
+})(OriginTypeCode || (OriginTypeCode = {}));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiT3JpZ2luVHlwZUNvZGUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi9wcm9qZWN0cy9uZy1jb3JlLWxpYi9zcmMvbGliL2VudW1zL09yaWdpblR5cGVDb2RlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOztFQUVFO0FBQ0YsTUFBTSxDQUFOLElBQVksY0FjWDtBQWRELFdBQVksY0FBYztJQUV0QixpQkFBaUI7SUFDakIsNkRBQWEsQ0FBQTtJQUViLDZCQUE2QjtJQUM3QixxREFBUyxDQUFBO0lBRVQsaUNBQWlDO0lBQ2pDLDZEQUFhLENBQUE7SUFFYix1Q0FBdUM7SUFDdkMsK0RBQWMsQ0FBQTtBQUVsQixDQUFDLEVBZFcsY0FBYyxLQUFkLGNBQWMsUUFjekIiLCJzb3VyY2VzQ29udGVudCI6WyIvKiBcbiAgIE9yaWdpbiB0eXBlIGNvZGUgXG4qL1xuZXhwb3J0IGVudW0gT3JpZ2luVHlwZUNvZGUge1xuIFxuICAgIC8vIFVuZGVmaW5lZCBbMF0gXG4gICAgVU5ERUZJTkVEID0gMCxcbiBcbiAgICAvLyBSZWdpc3RyYXRpb24gYnkgRW1haWwgWzFdIFxuICAgIEVNQUlMID0gMSxcbiBcbiAgICAvLyBSZWdpc3RyYXRpb24gYnkgTERBUCB1c2VyIFsyXSBcbiAgICBMREFQX1VTRVIgPSAyLFxuIFxuICAgIC8vIFJlZ2lzdHJhdGlvbiBieSBMREFQIHVzZXIgZ3JvdXAgWzNdIFxuICAgIExEQVBfR1JPVVAgPSAzLFxuIFxufSJdfQ==
