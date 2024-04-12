2e42166438db51e70ab0f49460f082912248145d
Circle CI <circleci@agentvi.com>
2024-04-12 11:00:40 +0000

    Update lib: 2.2.feature/inv-8447-store-manifests-in-postgres-3421

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,8 +1,8 @@
-import { TimeFrame } from '../common/TimeFrame';
-import { SearchObject } from '../common/SearchObject';
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchObject } from '../common/SearchObject';
 import { BaseEntity } from '../entities/BaseEntity';
 import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
