d2cb0f69daede169d69997e58b3463cfb7125c93
Circle CI <circleci@agentvi.com>
2024-04-12 10:52:26 +0000

    Update lib: 2.2.feature/inv-8447-store-manifests-in-postgres-3419

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
-import { SearchBehavior } from '../common/SearchBehavior';
-import { SearchObject } from '../common/SearchObject';
-import { ObjectData } from '../common/ObjectData';
-import { BaseEntity } from '../entities/BaseEntity';
+import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
-import { Coordinate } from '../common/Coordinate';
+import { ObjectData } from '../common/ObjectData';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchObject } from '../common/SearchObject';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
