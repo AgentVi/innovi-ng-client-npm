4ee3f6003d09120b8273cb84078db995295b3ed8
Circle CI <circleci@agentvi.com>
2024-04-12 10:12:41 +0000

    Update lib: 2.2.feature/inv-8447-store-manifests-in-postgres-3417

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,9 +1,9 @@
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { Coordinate } from '../common/Coordinate';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { TimestampedCrop } from '../common/TimestampedCrop';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
+import { Coordinate } from '../common/Coordinate';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
 import { BaseEntity } from '../entities/BaseEntity';
