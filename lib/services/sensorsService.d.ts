04d3a14e6f11b466c88afbfed6a69e62ee22a312
Circle CI <circleci@agentvi.com>
2024-04-14 15:32:30 +0000

    Update lib: 2.2.fix/test-timezone-3432

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -4,11 +4,11 @@ import { Sensor } from '../entities/Sensor';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { RulePolygon } from '../common/RulePolygon';
-import { SensorsGroup } from '../common/SensorsGroup';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
+import { SensorsGroup } from '../common/SensorsGroup';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
