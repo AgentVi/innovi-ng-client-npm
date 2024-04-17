38d141ef9bcb66c87ddfce3bd49d213fafe15b48
Circle CI <circleci@agentvi.com>
2024-04-17 09:37:20 +0000

    Update lib: 2.2.feature/inv-8307-add-dynamic-attributes-3455

--- lib/entities/Configuration.d.ts
+++ lib/entities/Configuration.d.ts
@@ -1,5 +1,5 @@
-import { ProductTypeCode } from '../enums/ProductTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
+import { ProductTypeCode } from '../enums/ProductTypeCode';
 export declare class Configuration extends BaseEntity {
     description: string;
     currentVersion: string;
