a52d6d499f38becd9ea4f20517c59697adf5f9e8
Circle CI <circleci@agentvi.com>
2024-04-26 05:57:44 +0000

    Update lib: 2.2.release/2024.2-3536

--- lib/entities/Account.d.ts
+++ lib/entities/Account.d.ts
@@ -1,10 +1,10 @@
+import { HealthThresholds } from '../common/HealthThresholds';
+import { PlatformTypeCode } from '../enums/PlatformTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 import { AccountTypeCode } from '../enums/AccountTypeCode';
 import { AccountStatusCode } from '../enums/AccountStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { AccountSettings } from '../common/AccountSettings';
-import { HealthThresholds } from '../common/HealthThresholds';
-import { PlatformTypeCode } from '../enums/PlatformTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
 export declare class Account extends BaseEntity {
     name: string;
     key: string;
@@ -16,6 +16,7 @@ export declare class Account extends BaseEntity {
     geoLocation: Coordinate;
     timezone: string;
     expiredOn: number;
+    maxChannels: number;
     description: string;
     settings: AccountSettings;
     healthThresholds: HealthThresholds;
