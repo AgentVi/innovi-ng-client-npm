f76d3b952abd52f88835e98553448c2f8d7d4109
Circle CI <circleci@agentvi.com>
2024-05-13 10:39:54 +0000

    Update lib: 2.2.feature/create-bi-service-3629

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { Sensor } from '../entities/Sensor';
-import { Coordinate } from '../common/Coordinate';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Preset } from '../entities/Preset';
+import { Sensor } from '../entities/Sensor';
 import { SensorsGroup } from '../common/SensorsGroup';
+import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
