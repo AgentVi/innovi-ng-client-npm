dcc54ae42750879dcf8b490be028033c532ba872
Circle CI <circleci@agentvi.com>
2024-05-14 10:50:13 +0000

    Update lib: 2.2.feature/create-bi-service-3637

--- lib/entities/FindUser.d.ts
+++ lib/entities/FindUser.d.ts
@@ -1,4 +1,3 @@
-import { OriginTypeCode } from '../enums/OriginTypeCode';
 import { UserTypeCode } from '../enums/UserTypeCode';
 import { UserStatusCode } from '../enums/UserStatusCode';
 import { FindUserAccountRole } from '../entities/FindUserAccountRole';
@@ -13,7 +12,5 @@ export declare class FindUser {
     accountRoles: FindUserAccountRole[];
     defaultAccount: string;
     description: string;
-    originId: string;
-    originType: OriginTypeCode;
-    constructor(id?: string, name?: string, email?: string, mobile?: string, type?: UserTypeCode, status?: UserStatusCode, lastSignIn?: number, accountRoles?: FindUserAccountRole[], defaultAccount?: string, description?: string, originId?: string, originType?: OriginTypeCode);
+    constructor(id?: string, name?: string, email?: string, mobile?: string, type?: UserTypeCode, status?: UserStatusCode, lastSignIn?: number, accountRoles?: FindUserAccountRole[], defaultAccount?: string, description?: string);
 }
--- lib/entities/User.d.ts
+++ lib/entities/User.d.ts
@@ -1,4 +1,3 @@
-import { OriginTypeCode } from '../enums/OriginTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { UserTypeCode } from '../enums/UserTypeCode';
 import { UserStatusCode } from '../enums/UserStatusCode';
@@ -13,8 +12,6 @@ export declare class User extends BaseEntity {
     defaultAccount: string;
     description: string;
     acceptedEula: boolean;
-    originId: string;
-    originType: OriginTypeCode;
     id: string;
     _type: string;
     createdOn: number;
