16dfa1190bd5ad4fb311045299f8174ef6d2a93c
Circle CI <circleci@agentvi.com>
2024-05-14 14:19:05 +0000

    Update lib: 2.2.feature/create-bi-service-3642

--- lib/messages/EntityResponseOfLicense.d.ts
+++ lib/messages/EntityResponseOfLicense.d.ts
@@ -1,5 +1,5 @@
-import { License } from '../entities/License';
 import { EntityResponse } from '../messages/EntityResponse';
+import { License } from '../entities/License';
 export declare class EntityResponseOfLicense extends EntityResponse {
     entity: License;
     code: number;
