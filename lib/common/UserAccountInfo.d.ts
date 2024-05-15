7566570e861403e5fe6928886bde5ad572ccaf4d
Circle CI <circleci@agentvi.com>
2024-05-15 16:22:02 +0000

    Update lib: 2.2.feature/inv-8780-feature-toggles-for-all-built-in-attributes-3649

--- lib/common/UserAccountInfo.d.ts
+++ lib/common/UserAccountInfo.d.ts
@@ -1,10 +1,9 @@
-import { FeatureCode } from '../enums/FeatureCode';
 import { UserAccountInfoAccount } from '../common/UserAccountInfoAccount';
 import { LoginData } from '../common/LoginData';
 export declare class UserAccountInfo {
     account: UserAccountInfoAccount;
     loginData: LoginData;
-    features: FeatureCode[];
+    features: string[];
     licenseExpiresOn: number;
-    constructor(account?: UserAccountInfoAccount, loginData?: LoginData, features?: FeatureCode[], licenseExpiresOn?: number);
+    constructor(account?: UserAccountInfoAccount, loginData?: LoginData, features?: string[], licenseExpiresOn?: number);
 }
--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
 import { TimeFrame } from '../common/TimeFrame';
-import { Coordinate } from '../common/Coordinate';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { Coordinate } from '../common/Coordinate';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
