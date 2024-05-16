84892731b6d97f4e1d9bebfbe8de5eb9a7f87dad
Circle CI <circleci@agentvi.com>
2024-05-16 10:35:13 +0000

    Update lib: 2.2.feature/inv-8925-endpoints-for-dynamic-features-3653

--- lib/messages/EntityResponseOfFeature.d.ts
+++
@@ -1,7 +0,0 @@
-import { Feature } from '../entities/Feature';
-import { EntityResponse } from '../messages/EntityResponse';
-export declare class EntityResponseOfFeature extends EntityResponse {
-    entity: Feature;
-    code: number;
-    error: string;
-}
