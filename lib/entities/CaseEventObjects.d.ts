9491402d3c0bfd9f5c4d35956a8ae9893abc37db
Circle CI <circleci@agentvi.com>
2024-05-20 10:03:23 +0000

    Update lib: 2.2.feature/bi-service-context-logger-3674

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { Coordinate } from '../common/Coordinate';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { ObjectInfo } from '../common/ObjectInfo';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { BoundingBox } from '../common/BoundingBox';
-import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BaseEntity } from '../entities/BaseEntity';
 import { EventStatusCode } from '../enums/EventStatusCode';
+import { Coordinate } from '../common/Coordinate';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { ObjectInfo } from '../common/ObjectInfo';
+import { TimestampedCrop } from '../common/TimestampedCrop';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
