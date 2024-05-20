3a11a346c756dd5212c04645b052249145624ad1
Circle CI <circleci@agentvi.com>
2024-05-20 11:02:30 +0000

    Update lib: 2.2.feature/inv-8623-change-event-report-to-show-username-3678

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Coordinate } from '../common/Coordinate';
 import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
--- lib/messages/EntityResponseOfConfiguration.d.ts
+++ lib/messages/EntityResponseOfConfiguration.d.ts
@@ -1,5 +1,5 @@
-import { Configuration } from '../entities/Configuration';
 import { EntityResponse } from '../messages/EntityResponse';
+import { Configuration } from '../entities/Configuration';
 export declare class EntityResponseOfConfiguration extends EntityResponse {
     entity: Configuration;
     code: number;
