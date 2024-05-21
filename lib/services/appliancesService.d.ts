9573701f714d6602972c09423adc155e928eda8d
Circle CI <circleci@agentvi.com>
2024-05-21 12:45:03 +0000

    Update lib: 2.2.fix/remove-unused-data-3696

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,15 +1,15 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { Sensor } from '../entities/Sensor';
-import { ProductTypeCode } from '../enums/ProductTypeCode';
-import { Appliance } from '../entities/Appliance';
-import { CommandStatusCode } from '../enums/CommandStatusCode';
 import { AgentStatusCode } from '../enums/AgentStatusCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { ProductTypeCode } from '../enums/ProductTypeCode';
+import { CommandStatusCode } from '../enums/CommandStatusCode';
+import { ApplianceRegistration } from '../common/ApplianceRegistration';
+import { Appliance } from '../entities/Appliance';
+import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { ApplianceRegistration } from '../common/ApplianceRegistration';
+import { Sensor } from '../entities/Sensor';
 import * as i0 from "@angular/core";
 /**
  * List of appliance related actions
