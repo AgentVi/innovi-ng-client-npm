5202435bcd41b02af88e7bad5c7840f6ecacfcb2
Circle CI <circleci@agentvi.com>
2024-05-23 06:01:50 +0000

    Update lib: 2.2.feature/inv-8449-ensure-model-feature-2-3733

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,14 +1,14 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { Sensor } from '../entities/Sensor';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { Preset } from '../entities/Preset';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { RulePolygon } from '../common/RulePolygon';
 import { SensorsGroup } from '../common/SensorsGroup';
-import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Sensor } from '../entities/Sensor';
+import { Coordinate } from '../common/Coordinate';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
