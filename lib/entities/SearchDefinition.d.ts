b9eed7fc5fbe96ff827208df2a6a896af0f416ee
Circle CI <circleci@agentvi.com>
2024-05-27 05:24:23 +0000

    Update lib: 2.2.feature/inv-7839-personalized-ai-phase-1-3752

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,9 +1,9 @@
 import { Coordinate } from '../common/Coordinate';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchObject } from '../common/SearchObject';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
-import { SearchObject } from '../common/SearchObject';
-import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
