0e3fd3abd8928d144b206df332ba259e9b2676fd
Circle CI <circleci@agentvi.com>
2024-05-30 07:20:34 +0000

    Update lib: 2.2.feature/inv-7839-personalized-ai-phase-1-3790

--- lib/messages/EntityResponseOfFeature.d.ts
+++
@@ -1,7 +0,0 @@
-import { EntityResponse } from '../messages/EntityResponse';
-import { Feature } from '../entities/Feature';
-export declare class EntityResponseOfFeature extends EntityResponse {
-    entity: Feature;
-    code: number;
-    error: string;
-}
