2411619e40fffddcf0802d9139166fc3d820373d
Circle CI <circleci@agentvi.com>
2024-05-31 14:40:20 +0000

    Update lib: 2.2.feature/inv-8872-backwards-compatibility-3806

--- lib/common/SearchShape.d.ts
+++ lib/common/SearchShape.d.ts
@@ -1,7 +1,7 @@
-import { RulePolygon } from '../common/RulePolygon';
-import { LineCrossDirectionCode } from '../enums/LineCrossDirectionCode';
 import { Point } from '../common/Point';
 import { RuleColorCode } from '../enums/RuleColorCode';
+import { RulePolygon } from '../common/RulePolygon';
+import { LineCrossDirectionCode } from '../enums/LineCrossDirectionCode';
 export declare class SearchShape extends RulePolygon {
     sensorId: string;
     lineCrossDir: LineCrossDirectionCode;
