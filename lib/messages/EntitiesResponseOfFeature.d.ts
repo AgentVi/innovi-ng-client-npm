8c56e4127fcd01b2c53c5d22e819fd5bdd70bc3a
Circle CI <circleci@agentvi.com>
2024-06-03 11:55:54 +0000

    Update lib: 2.2.feature/inv-8872-backwards-compatibility-3815

--- lib/messages/EntitiesResponseOfFeature.d.ts
+++
@@ -1,7 +0,0 @@
-import { Feature } from '../entities/Feature';
-import { EntitiesResponse } from '../messages/EntitiesResponse';
-export declare class EntitiesResponseOfFeature extends EntitiesResponse {
-    list: Feature[];
-    code: number;
-    error: string;
-}
