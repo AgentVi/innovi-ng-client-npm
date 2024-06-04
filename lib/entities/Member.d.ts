1a185deba75f04c503f8bf9655acae77e24c5d3e
Circle CI <circleci@agentvi.com>
2024-06-04 13:22:07 +0000

    Update lib: 2.2.feature/inv-8622-mcc-add-ad-search-to-user-invite-3833

--- lib/entities/Member.d.ts
+++ lib/entities/Member.d.ts
@@ -1,3 +1,4 @@
+import { OriginTypeCode } from '../enums/OriginTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { AccountRoleCode } from '../enums/AccountRoleCode';
 import { UserTypeCode } from '../enums/UserTypeCode';
@@ -13,6 +14,8 @@ export declare class Member extends BaseEntity {
     mobile: string;
     type: UserTypeCode;
     status: UserStatusCode;
+    originId: string;
+    originType: OriginTypeCode;
     id: string;
     _type: string;
     createdOn: number;
