094e0dbf416bc317aa4be50dbb77a4dac0fa73f8
Circle CI <circleci@agentvi.com>
2024-06-11 15:46:20 +0000

    Update lib: 2.2.feature/expanded-docker-compose-3870

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,13 +1,13 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
 import { SensorsGroup } from '../common/SensorsGroup';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { Sensor } from '../entities/Sensor';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
-import { Sensor } from '../entities/Sensor';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
 /**
