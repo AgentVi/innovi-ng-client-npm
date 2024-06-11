02c87e491411e417770bc5b6f6c4c3dc2fed58b2
Circle CI <circleci@agentvi.com>
2024-06-11 06:40:42 +0000

    Update lib: 2.2.fix/inv-7839-handle-no-row-found-error-in-postgres-delete-3859

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { SearchBehavior } from '../common/SearchBehavior';
-import { Coordinate } from '../common/Coordinate';
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { Coordinate } from '../common/Coordinate';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
