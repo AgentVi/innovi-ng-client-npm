df25ebffd394d00d885ae1ca15b3c22b27522565
Circle CI <circleci@agentvi.com>
2024-06-16 18:43:30 +0000

    Update lib: 2.2.feature/inv-8622-mcc-add-ad-search-to-user-invite-3890

---
+++ lib/entities/FeaturesGroup.d.ts
@@ -0,0 +1,11 @@
+import { BaseEntity } from '../entities/BaseEntity';
+import { FeatureCode } from '../enums/FeatureCode';
+export declare class FeaturesGroup extends BaseEntity {
+    name: string;
+    features: FeatureCode[];
+    id: string;
+    _type: string;
+    createdOn: number;
+    updatedOn: number;
+    path: string;
+}
