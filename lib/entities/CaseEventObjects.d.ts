4d983d88015d85a8b497e4917f5446d3025fba2c
Circle CI <circleci@agentvi.com>
2024-06-19 20:19:02 +0000

    Update lib: 2.2.fix/inv-9134-cursor-elastic-3918

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,11 +1,11 @@
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { Coordinate } from '../common/Coordinate';
 import { ObjectInfo } from '../common/ObjectInfo';
-import { BaseEntity } from '../entities/BaseEntity';
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { BoundingBox } from '../common/BoundingBox';
+import { BaseEntity } from '../entities/BaseEntity';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { Coordinate } from '../common/Coordinate';
 import { TimestampedCrop } from '../common/TimestampedCrop';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
