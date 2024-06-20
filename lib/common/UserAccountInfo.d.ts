62d53a965a9d720be5e1319a897f193c1b769f19
Circle CI <circleci@agentvi.com>
2024-06-20 19:35:29 +0000

    Update lib: 2.2.fix/inv-9134-cursor-elastic-3929

--- lib/common/UserAccountInfo.d.ts
+++ lib/common/UserAccountInfo.d.ts
@@ -1,9 +1,10 @@
-import { LoginData } from '../common/LoginData';
+import { FeatureCode } from '../enums/FeatureCode';
 import { UserAccountInfoAccount } from '../common/UserAccountInfoAccount';
+import { LoginData } from '../common/LoginData';
 export declare class UserAccountInfo {
     account: UserAccountInfoAccount;
     loginData: LoginData;
-    features: string[];
+    features: FeatureCode[];
     licenseExpiresOn: number;
-    constructor(account?: UserAccountInfoAccount, loginData?: LoginData, features?: string[], licenseExpiresOn?: number);
+    constructor(account?: UserAccountInfoAccount, loginData?: LoginData, features?: FeatureCode[], licenseExpiresOn?: number);
 }
--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { Coordinate } from '../common/Coordinate';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
+import { Coordinate } from '../common/Coordinate';
 import { StringKeyValue } from '../common/StringKeyValue';
 import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
