484478914677ee4a19e59dce999a2b8fc0e2bf28
Circle CI <circleci@agentvi.com>
2024-06-21 11:06:33 +0000

    Update lib: 2.2.fix/inv-7839-could-not-parse-behavior-type-param-3930

--- lib/messages/FeaturesServiceFindRequest.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class FeaturesServiceFindRequest {
-    search: string;
-    category: string;
-    sort: string;
-    constructor(search?: string, category?: string, sort?: string);
-}
