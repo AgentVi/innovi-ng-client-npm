a17073422bbaf2b32e319bba6f27ff9cc50bbe0a
Circle CI <circleci@agentvi.com>
2024-06-24 09:25:37 +0000

    Update lib: 2.2.fix/inv-9134-cursor-elastic-3937

--- lib/services/appliancesService.d.ts
+++ lib/services/appliancesService.d.ts
@@ -1,11 +1,11 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
+import { Appliance } from '../entities/Appliance';
 import { AgentStatusCode } from '../enums/AgentStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Sensor } from '../entities/Sensor';
 import { ApplianceRegistration } from '../common/ApplianceRegistration';
 import { ProductTypeCode } from '../enums/ProductTypeCode';
-import { Appliance } from '../entities/Appliance';
 import { CommandStatusCode } from '../enums/CommandStatusCode';
 import { ApplianceStatusCode } from '../enums/ApplianceStatusCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
