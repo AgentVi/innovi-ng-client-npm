0ac904c563a0dc1353621cc3e3a510222f478ffe
Circle CI <circleci@agentvi.com>
2024-07-10 16:24:10 +0000

    Update lib: 2.2.feature/IRPE-6-health-statuses-3973

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StringKeyValue } from '../common/StringKeyValue';
-import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
 import { Thresholds } from '../common/Thresholds';
+import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
@@ -49,6 +49,7 @@ export declare class Sensor extends BaseEntity {
     eventClipLengthSec: number;
     personRecognition: boolean;
     anonymizationType: SensorAnonymizationTypeCode;
+    trackerImprovementInterval: number;
     id: string;
     _type: string;
     createdOn: number;
--- lib/messages/EntitiesResponseOfFeaturesGroup.d.ts
+++
@@ -1,7 +0,0 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
-import { FeaturesGroup } from '../entities/FeaturesGroup';
-export declare class EntitiesResponseOfFeaturesGroup extends EntitiesResponse {
-    list: FeaturesGroup[];
-    code: number;
-    error: string;
-}
