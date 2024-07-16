a87a140d164437792072a7483f5415d69365ca53
Circle CI <circleci@agentvi.com>
2024-07-16 12:33:18 +0000

    Update lib: 2.2.fix/migration-rule_v2-3985

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Thresholds } from '../common/Thresholds';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { StringKeyValue } from '../common/StringKeyValue';
+import { Thresholds } from '../common/Thresholds';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { StringKeyValue } from '../common/StringKeyValue';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
 export declare class Sensor extends BaseEntity {
