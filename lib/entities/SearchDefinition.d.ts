ff39a3be91b85d998653441cfe26a943c5e26288
Circle CI <circleci@agentvi.com>
2024-07-21 06:38:15 +0000

    Update lib: 2.2.feature/lyain-irpe-150-validate-auditlog-routes-4011

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { BaseEntity } from '../entities/BaseEntity';
-import { Coordinate } from '../common/Coordinate';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
-import { ObjectData } from '../common/ObjectData';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchBehavior } from '../common/SearchBehavior';
+import { ObjectData } from '../common/ObjectData';
+import { Coordinate } from '../common/Coordinate';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchObject } from '../common/SearchObject';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
--- lib/messages/FeaturesGroupsServiceSetNameRequest.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class FeaturesGroupsServiceSetNameRequest {
-    id: string;
-    body: string;
-    constructor(id?: string, body?: string);
-}
