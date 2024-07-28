37feedda1de2cbbbf7b06564d8fe02cfa0877af1
Circle CI <circleci@agentvi.com>
2024-07-28 13:53:22 +0000

    Update lib: 2.2.feature/IRPE-6-health-statuses-4023

--- lib/entities/FindUser.d.ts
+++ lib/entities/FindUser.d.ts
@@ -1,5 +1,4 @@
 import { FindUserAccountRole } from '../entities/FindUserAccountRole';
-import { OriginTypeCode } from '../enums/OriginTypeCode';
 import { UserTypeCode } from '../enums/UserTypeCode';
 import { UserStatusCode } from '../enums/UserStatusCode';
 export declare class FindUser {
@@ -13,7 +12,5 @@ export declare class FindUser {
     accountRoles: FindUserAccountRole[];
     defaultAccount: string;
     description: string;
-    originId: string;
-    originType: OriginTypeCode;
-    constructor(id?: string, name?: string, email?: string, mobile?: string, type?: UserTypeCode, status?: UserStatusCode, lastSignIn?: number, accountRoles?: FindUserAccountRole[], defaultAccount?: string, description?: string, originId?: string, originType?: OriginTypeCode);
+    constructor(id?: string, name?: string, email?: string, mobile?: string, type?: UserTypeCode, status?: UserStatusCode, lastSignIn?: number, accountRoles?: FindUserAccountRole[], defaultAccount?: string, description?: string);
 }
--- lib/messages/FeaturesServiceUpdateRequest.d.ts
+++
@@ -1,6 +0,0 @@
-import { Feature } from '../entities/Feature';
-export declare class FeaturesServiceUpdateRequest {
-    id: string;
-    body: Feature;
-    constructor(id?: string, body?: Feature);
-}
