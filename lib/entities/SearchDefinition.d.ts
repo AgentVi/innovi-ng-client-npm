4ddf3588e0995dff8363d0cd3465e9bccac3b9fb
Circle CI <circleci@agentvi.com>
2024-07-29 13:40:28 +0000

    Update lib: 2.2.4027

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { SearchObject } from '../common/SearchObject';
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
+import { ObjectData } from '../common/ObjectData';
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchBehavior } from '../common/SearchBehavior';
-import { ObjectData } from '../common/ObjectData';
+import { SearchObject } from '../common/SearchObject';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
