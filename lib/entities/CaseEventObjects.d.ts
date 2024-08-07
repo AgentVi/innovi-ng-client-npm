15c5df71ed9c78ad7fdc729009279cb31c46fb92
Circle CI <circleci@agentvi.com>
2024-08-07 10:00:40 +0000

    Update lib: 2.2.feature/pai-phase2-4075

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,8 +1,8 @@
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { Coordinate } from '../common/Coordinate';
 import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
+import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
