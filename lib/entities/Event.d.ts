37bb3100b4948fd844a809d3120e0fdeef73836f
Circle CI <circleci@agentvi.com>
2024-08-15 13:49:52 +0000

    Update lib: 2.2.feature/IRPE-6-health-statuses-4100

--- lib/entities/Event.d.ts
+++ lib/entities/Event.d.ts
@@ -1,3 +1,4 @@
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
@@ -5,7 +6,6 @@ import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
 import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 export declare class Event extends BaseEntity {
     sensorId: string;
     extSensorId: string;
