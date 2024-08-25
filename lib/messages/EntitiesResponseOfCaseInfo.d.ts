51a7907cdf1c35198283089cbfc7a56f2698c266
Circle CI <circleci@agentvi.com>
2024-08-25 07:26:23 +0000

    Update lib: 2.2.fix/irpe-407-unatnnted-object-ex-model-alarm-filter-model-4144

--- lib/messages/EntitiesResponseOfCaseInfo.d.ts
+++ lib/messages/EntitiesResponseOfCaseInfo.d.ts
@@ -1,5 +1,5 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
 import { CaseInfo } from '../entities/CaseInfo';
+import { EntitiesResponse } from '../messages/EntitiesResponse';
 export declare class EntitiesResponseOfCaseInfo extends EntitiesResponse {
     list: CaseInfo[];
     code: number;
