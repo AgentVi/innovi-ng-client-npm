9043faf11f51a82cc73a11e4273e873310161ef8
Circle CI <circleci@agentvi.com>
2024-09-02 10:16:47 +0000

    Update lib: 2.2.4206

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { BoundingBox } from '../common/BoundingBox';
+import { Coordinate } from '../common/Coordinate';
 import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BaseEntity } from '../entities/BaseEntity';
 import { EventStatusCode } from '../enums/EventStatusCode';
-import { Coordinate } from '../common/Coordinate';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
+import { BoundingBox } from '../common/BoundingBox';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
