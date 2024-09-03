6a6ae6fc19f419705062d949645af7de794b97f5
Circle CI <circleci@agentvi.com>
2024-09-03 19:13:32 +0000

    Update lib: 2.2.feature/pai-phase2-4211

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
+import { Thresholds } from '../common/Thresholds';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class Sensor extends BaseEntity {
--- lib/messages/EntityResponseOfDetectionModel.d.ts
+++ lib/messages/EntityResponseOfDetectionModel.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { DetectionModel } from '../entities/DetectionModel';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfDetectionModel extends EntityResponse {
     entity: DetectionModel;
     code: number;
