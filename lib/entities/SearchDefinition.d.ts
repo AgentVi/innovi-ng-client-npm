230b75fde43c875a6f640942a5f179ef5909b374
Circle CI <circleci@agentvi.com>
2024-09-05 17:21:37 +0000

    Update lib: 2.2.fix/IRPE-421-attribute-mismatch-4235

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
+import { SearchObject } from '../common/SearchObject';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
-import { SearchObject } from '../common/SearchObject';
-import { ObjectData } from '../common/ObjectData';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchBehavior } from '../common/SearchBehavior';
-import { BaseEntity } from '../entities/BaseEntity';
+import { ObjectData } from '../common/ObjectData';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
