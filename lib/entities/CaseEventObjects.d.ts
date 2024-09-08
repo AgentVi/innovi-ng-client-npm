69af68fc071873f3e461635ad156066419490859
Circle CI <circleci@agentvi.com>
2024-09-08 13:07:39 +0000

    Update lib: 2.2.fix/IRPE-421-attribute-mismatch-4239

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,10 +1,10 @@
+import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
-import { TimestampedCrop } from '../common/TimestampedCrop';
-import { BaseEntity } from '../entities/BaseEntity';
+import { EventStatusCode } from '../enums/EventStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { ObjectInfo } from '../common/ObjectInfo';
+import { TimestampedCrop } from '../common/TimestampedCrop';
+import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 export declare class CaseEventObjects extends BaseEntity {
