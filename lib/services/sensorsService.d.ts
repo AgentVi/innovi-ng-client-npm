11c120b7eb5a5de7b382280a040701db66cdbdf0
Circle CI <circleci@agentvi.com>
2024-09-29 05:02:33 +0000

    Update lib: 2.2.feature/lyalin-irpe-467-unify-health-statuses-sources-4311

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,10 +1,10 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
 import { SensorsGroup } from '../common/SensorsGroup';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
 import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
