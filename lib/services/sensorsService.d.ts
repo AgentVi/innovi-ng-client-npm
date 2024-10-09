53e81b89c7b67bad7d8b6649bf7f4ab8d7e8e938
Circle CI <circleci@agentvi.com>
2024-10-09 15:26:46 +0000

    Update lib: 2.2.feature/IRPE-572-add-debug-method-to-new-logger-4336

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,14 +1,14 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
 import { SensorsGroup } from '../common/SensorsGroup';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { Preset } from '../entities/Preset';
-import { RulePolygon } from '../common/RulePolygon';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Sensor } from '../entities/Sensor';
 import { Coordinate } from '../common/Coordinate';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Preset } from '../entities/Preset';
+import { Sensor } from '../entities/Sensor';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { RulePolygon } from '../common/RulePolygon';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
