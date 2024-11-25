ae4e2e67abf26b57cdd9c7243a93775bebd86851
Circle CI <circleci@agentvi.com>
2024-11-25 12:36:52 +0000

    Update lib: 2.2.fix/IRPE-693-core-api-logging-fix-4465

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { Preset } from '../entities/Preset';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
 import { SensorsGroup } from '../common/SensorsGroup';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Preset } from '../entities/Preset';
+import { RulePolygon } from '../common/RulePolygon';
 import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import * as i0 from "@angular/core";
