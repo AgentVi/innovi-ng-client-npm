ed061d6ecfacfb289ef7f7c8ac6a0c6ee4e5e1c1
Circle CI <circleci@agentvi.com>
2024-12-10 17:19:54 +0000

    Update lib: 2.2.fix/IRPE-708-email-integ-4529

--- lib/entities/ObjectsPath.d.ts
+++ lib/entities/ObjectsPath.d.ts
@@ -1,6 +1,6 @@
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { Point } from '../common/Point';
 import { ObjectTypeCount } from '../entities/ObjectTypeCount';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 export declare class ObjectsPath {
     id: number;
     count: number;
