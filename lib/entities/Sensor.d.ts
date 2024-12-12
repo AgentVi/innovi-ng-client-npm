f367aebea17e85b261fce575c90ad91bef18f791
Circle CI <circleci@agentvi.com>
2024-12-12 15:05:43 +0000

    Update lib: 2.2.4539

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,14 +1,14 @@
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { Coordinate } from '../common/Coordinate';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Thresholds } from '../common/Thresholds';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { Coordinate } from '../common/Coordinate';
-import { StringKeyValue } from '../common/StringKeyValue';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class Sensor extends BaseEntity {
     accountId: string;
--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,13 +1,13 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { RulePolygon } from '../common/RulePolygon';
-import { SensorsGroup } from '../common/SensorsGroup';
-import { Coordinate } from '../common/Coordinate';
+import { Sensor } from '../entities/Sensor';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Preset } from '../entities/Preset';
-import { Sensor } from '../entities/Sensor';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { RulePolygon } from '../common/RulePolygon';
+import { SensorsGroup } from '../common/SensorsGroup';
+import { Coordinate } from '../common/Coordinate';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import * as i0 from "@angular/core";
 /**
