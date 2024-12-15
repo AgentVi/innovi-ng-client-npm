e1e0e814f6f725113c0fc2a51d949e6e35b1763f
Circle CI <circleci@agentvi.com>
2024-12-15 10:26:26 +0000

    Update lib: 2.2.fix/lyalin-fix-scheduled-reports-generation-4543

--- lib/messages/EntitiesResponseOfApplianceConfiguration.d.ts
+++ lib/messages/EntitiesResponseOfApplianceConfiguration.d.ts
@@ -1,5 +1,5 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
 import { ApplianceConfiguration } from '../common/ApplianceConfiguration';
+import { EntitiesResponse } from '../messages/EntitiesResponse';
 export declare class EntitiesResponseOfApplianceConfiguration extends EntitiesResponse {
     list: ApplianceConfiguration[];
     code: number;
