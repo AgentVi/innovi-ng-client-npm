d8a915ce45e6eee102139abeb7e5d24be4f259a5
Circle CI <circleci@agentvi.com>
2024-12-15 13:02:29 +0000

    Update lib: 2.2.feature/IRPE-765-gateway-parse-error-logging-4545

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { ObjectData } from '../common/ObjectData';
 import { BaseEntity } from '../entities/BaseEntity';
+import { ObjectData } from '../common/ObjectData';
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { TimeFrame } from '../common/TimeFrame';
-import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchObject } from '../common/SearchObject';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
