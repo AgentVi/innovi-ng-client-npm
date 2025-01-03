24333c449684a97826e1efc68b9080b02ab9cc55
Circle CI <circleci@agentvi.com>
2025-01-03 21:43:08 +0000

    Update lib: 2.2.fix/IRPE-785-step3-4596

--- lib/messages/EntityResponseOfSensorAnomalyInfo.d.ts
+++ lib/messages/EntityResponseOfSensorAnomalyInfo.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { SensorAnomalyInfo } from '../common/SensorAnomalyInfo';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfSensorAnomalyInfo extends EntityResponse {
     entity: SensorAnomalyInfo;
     code: number;
