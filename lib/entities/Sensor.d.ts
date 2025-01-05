28fc1c764e9a0b1eface34d61443400ae1d8568f
Circle CI <circleci@agentvi.com>
2025-01-05 13:23:56 +0000

    Update lib: 2.2.feature/irpe-161-remove-unused-sensor-settings-4606

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
 import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StringKeyValue } from '../common/StringKeyValue';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
-import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
+import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { Thresholds } from '../common/Thresholds';
+import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
@@ -44,7 +44,6 @@ export declare class Sensor extends BaseEntity {
     framerate: number;
     highSensitivity: boolean;
     viewType: ViewTypeCode;
-    poseEstimation: boolean;
     eventFiltering: boolean;
     eventClipLengthSec: number;
     personRecognition: boolean;
--- lib/messages/EntityResponseOfApplianceDiscovery.d.ts
+++ lib/messages/EntityResponseOfApplianceDiscovery.d.ts
@@ -1,5 +1,5 @@
-import { ApplianceDiscovery } from '../entities/ApplianceDiscovery';
 import { EntityResponse } from '../messages/EntityResponse';
+import { ApplianceDiscovery } from '../entities/ApplianceDiscovery';
 export declare class EntityResponseOfApplianceDiscovery extends EntityResponse {
     entity: ApplianceDiscovery;
     code: number;
