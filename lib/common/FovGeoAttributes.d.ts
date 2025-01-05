3e4947e9ec7a99fc71f952d18aaafd3a5bf87716
Circle CI <circleci@agentvi.com>
2025-01-05 09:54:52 +0000

    Update lib: 2.2.feature/irpe-161-remove-unused-sensor-settings-4597

--- lib/common/FovGeoAttributes.d.ts
+++ lib/common/FovGeoAttributes.d.ts
@@ -1,7 +1,7 @@
-import { Dimension } from '../common/Dimension';
 import { TransformationTypeCode } from '../enums/TransformationTypeCode';
 import { GeoControlPoint } from '../common/GeoControlPoint';
 import { Coordinate } from '../common/Coordinate';
+import { Dimension } from '../common/Dimension';
 export declare class FovGeoAttributes {
     geoArea: Coordinate[];
     visibleArea: Dimension;
