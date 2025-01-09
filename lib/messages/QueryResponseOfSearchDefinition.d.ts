8b300dbe0d6a06915eafb737e870ee16a38b5c57
Circle CI <circleci@agentvi.com>
2025-01-09 09:18:46 +0000

    Update lib: 2.2.feature/pai-phase2-4635

--- lib/messages/QueryResponseOfSearchDefinition.d.ts
+++ lib/messages/QueryResponseOfSearchDefinition.d.ts
@@ -1,5 +1,5 @@
-import { QueryResponse } from '../messages/QueryResponse';
 import { SearchDefinition } from '../entities/SearchDefinition';
+import { QueryResponse } from '../messages/QueryResponse';
 export declare class QueryResponseOfSearchDefinition extends QueryResponse {
     list: SearchDefinition[];
     code: number;
