ca7c02b03e65b21f8d70a2fe1b0d14ba31b1eb05
Circle CI <circleci@agentvi.com>
2025-01-12 10:01:48 +0000

    Update lib: 2.2.feature/pai-phase2-4637

--- lib/messages/EntityResponseOfTimestampedCropsData.d.ts
+++ lib/messages/EntityResponseOfTimestampedCropsData.d.ts
@@ -1,5 +1,5 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
 import { TimestampedCropsData } from '../common/TimestampedCropsData';
+import { EntitiesResponse } from '../messages/EntitiesResponse';
 export declare class EntityResponseOfTimestampedCropsData extends EntitiesResponse {
     entity: TimestampedCropsData;
     code: number;
