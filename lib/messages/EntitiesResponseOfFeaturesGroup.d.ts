476a4ab92ffff1ebfb59dc76fa1df5b761ca2df5
Circle CI <circleci@agentvi.com>
2025-01-14 08:06:23 +0000

    Update lib: 2.2.fix/remove-redundend-dev-log-4652

--- lib/messages/EntitiesResponseOfFeaturesGroup.d.ts
+++
@@ -1,7 +0,0 @@
-import { FeaturesGroup } from '../entities/FeaturesGroup';
-import { EntitiesResponse } from '../messages/EntitiesResponse';
-export declare class EntitiesResponseOfFeaturesGroup extends EntitiesResponse {
-    list: FeaturesGroup[];
-    code: number;
-    error: string;
-}
