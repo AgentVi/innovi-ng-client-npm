9bbec2feddb2249b9ac7d99a81223cb0d576aa28
Circle CI <circleci@agentvi.com>
2025-01-16 11:00:49 +0000

    Update lib: 2.2.feature/IRPE-835-broken-uploads-to-traffic-ratio-4668

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,10 +1,10 @@
-import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { BaseEntity } from '../entities/BaseEntity';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
 import { StringKeyValue } from '../common/StringKeyValue';
