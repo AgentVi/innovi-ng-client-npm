c750a19af23ee507513cd7d1b7351717466e53f9
Circle CI <circleci@agentvi.com>
2025-01-26 14:31:02 +0000

    Update lib: 2.2.feature/irpe-472-Insigts-add-event-status-as-data-source-4701

--- lib/messages/EntityResponseOfIntegrationAction.d.ts
+++ lib/messages/EntityResponseOfIntegrationAction.d.ts
@@ -1,5 +1,5 @@
-import { IntegrationAction } from '../entities/IntegrationAction';
 import { EntityResponse } from '../messages/EntityResponse';
+import { IntegrationAction } from '../entities/IntegrationAction';
 export declare class EntityResponseOfIntegrationAction extends EntityResponse {
     entity: IntegrationAction;
     code: number;
