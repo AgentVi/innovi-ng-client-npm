7983ebbf144f7dfaa4dfb11c87ddbb49925db548
Circle CI <circleci@agentvi.com>
2025-01-27 12:26:06 +0000

    Update lib: 2.2.feature/IRPE-863-log-event-time-diff-in-proxy-4707

--- lib/messages/EntityResponseOfHealthEvent.d.ts
+++ lib/messages/EntityResponseOfHealthEvent.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { HealthEvent } from '../entities/HealthEvent';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfHealthEvent extends EntityResponse {
     entity: HealthEvent;
     code: number;
