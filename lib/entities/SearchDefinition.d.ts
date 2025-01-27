d9399649e67230f2495b743fe922683b10890178
Circle CI <circleci@agentvi.com>
2025-01-27 11:49:48 +0000

    Update lib: 2.2.feature/IRPE-863-log-event-time-diff-in-proxy-4706

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
+import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { ObjectData } from '../common/ObjectData';
+import { Coordinate } from '../common/Coordinate';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchObject } from '../common/SearchObject';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
-import { TimeFrame } from '../common/TimeFrame';
-import { SearchBehavior } from '../common/SearchBehavior';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { ObjectData } from '../common/ObjectData';
-import { Coordinate } from '../common/Coordinate';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
