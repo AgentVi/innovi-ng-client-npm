3a9797dedfc28149fc7560dec60b58872a7fd958
Circle CI <circleci@agentvi.com>
2025-01-30 09:58:56 +0000

    Update lib: 2.2.release/2025.1-4721

--- lib/common/SensorInfo.d.ts
+++ lib/common/SensorInfo.d.ts
@@ -1,4 +1,3 @@
-import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
@@ -6,6 +5,7 @@ import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 export declare class SensorInfo {
     name: string;
     type: SensorTypeCode;
