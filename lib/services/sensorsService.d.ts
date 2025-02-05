655020ac1f720c85011d1cc254a910d224643602
Circle CI <circleci@agentvi.com>
2025-02-05 14:04:27 +0000

    Update lib: 2.2.feature/IRPE-863-log-event-time-diff-in-proxy-4742

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,7 +1,5 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { SensorsGroup } from '../common/SensorsGroup';
 import { Coordinate } from '../common/Coordinate';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
@@ -9,6 +7,8 @@ import { Preset } from '../entities/Preset';
 import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
