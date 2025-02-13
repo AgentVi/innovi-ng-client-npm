3a0efdf1c2ddd8828acd6e951b970e15f1442ff2
Circle CI <circleci@agentvi.com>
2025-02-13 06:07:29 +0000

    Update lib: 2.2.feature/IRPE-923-metadata-optimizations-4761

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
+import { SearchObject } from '../common/SearchObject';
+import { BaseEntity } from '../entities/BaseEntity';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchBehavior } from '../common/SearchBehavior';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
