087464c0e89d97fd9fa086b768dd4496d2fc606e
Circle CI <circleci@agentvi.com>
2025-02-13 14:22:26 +0000

    Update lib: 2.2.fix/irpe-928-change-identiv-platform-name-4767

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
+import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { TimestampedCrop } from '../common/TimestampedCrop';
-import { Coordinate } from '../common/Coordinate';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
-import { BaseEntity } from '../entities/BaseEntity';
+import { TimestampedCrop } from '../common/TimestampedCrop';
+import { EventStatusCode } from '../enums/EventStatusCode';
+import { Coordinate } from '../common/Coordinate';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
