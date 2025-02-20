982f801f8261c6a3cbfb3e7ad432fb2e8027a9a6
Circle CI <circleci@agentvi.com>
2025-02-20 14:54:29 +0000

    Update lib: 2.2.feature/IRPE-937-agent-upload-via-ws-4791

--- lib/messages/EntitiesResponseOfSensor.d.ts
+++ lib/messages/EntitiesResponseOfSensor.d.ts
@@ -1,5 +1,5 @@
-import { Sensor } from '../entities/Sensor';
 import { EntitiesResponse } from '../messages/EntitiesResponse';
+import { Sensor } from '../entities/Sensor';
 export declare class EntitiesResponseOfSensor extends EntitiesResponse {
     list: Sensor[];
     code: number;
