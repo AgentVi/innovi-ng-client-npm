4b886c2a7296306e0df93561c5fc77426357f4ec
Circle CI <circleci@agentvi.com>
2025-02-24 19:45:54 +0000

    Update lib: 2.2.fix/irpe-548-scheduled-reports-not-generating-new-4808

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { TimeFrame } from '../common/TimeFrame';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { Coordinate } from '../common/Coordinate';
 import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchObject } from '../common/SearchObject';
+import { Coordinate } from '../common/Coordinate';
+import { TimeFrame } from '../common/TimeFrame';
 import { ObjectData } from '../common/ObjectData';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
