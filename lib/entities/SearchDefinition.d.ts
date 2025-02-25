4360bd8f9cdbf8f0cf06fcc6ae2c4a30fd61532b
Circle CI <circleci@agentvi.com>
2025-02-25 21:32:06 +0000

    Update lib: 2.2.feature/irpe-873-implement-i18n-translations-4825

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { TimeFrame } from '../common/TimeFrame';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
-import { Coordinate } from '../common/Coordinate';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { TimeFrame } from '../common/TimeFrame';
+import { BaseEntity } from '../entities/BaseEntity';
+import { Coordinate } from '../common/Coordinate';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
