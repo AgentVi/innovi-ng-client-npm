98ee7c4ad46094cbed327c4788278fd4f509b81c
Circle CI <circleci@agentvi.com>
2025-02-28 13:14:36 +0000

    Update lib: 2.2.feature/irpe-873-implement-i18n-translations-4836

--- lib/messages/EntitiesResponseOfSensorStatus.d.ts
+++ lib/messages/EntitiesResponseOfSensorStatus.d.ts
@@ -1,5 +1,5 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
 import { SensorStatus } from '../entities/SensorStatus';
+import { EntitiesResponse } from '../messages/EntitiesResponse';
 export declare class EntitiesResponseOfSensorStatus extends EntitiesResponse {
     list: SensorStatus[];
     code: number;
