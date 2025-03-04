c9077c0b63fef5d086dc6b7bd758987b4850685a
Circle CI <circleci@agentvi.com>
2025-03-04 08:49:51 +0000

    Update lib: 2.2.fix/irpe-548-fix-inconsistency-in-core-reports-tests-4861

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { TimeFrame } from '../common/TimeFrame';
-import { SearchObject } from '../common/SearchObject';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
+import { SearchObject } from '../common/SearchObject';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
-import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { TimeFrame } from '../common/TimeFrame';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
