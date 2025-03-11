895d72fa36d14aaa7cbdb500412518592a17963c
Circle CI <circleci@agentvi.com>
2025-03-11 17:06:15 +0000

    Update lib: 2.2.fix/IRPE-962-no-event-time-in-integ-4880

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { SearchBehavior } from '../common/SearchBehavior';
-import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchObject } from '../common/SearchObject';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,13 +1,13 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
+import { RulePolygon } from '../common/RulePolygon';
 import { SensorsGroup } from '../common/SensorsGroup';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
 import * as i0 from "@angular/core";
 /**
