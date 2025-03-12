594fed5c642b95053b376e9cc909944d31f9ad1f
Circle CI <circleci@agentvi.com>
2025-03-12 13:31:26 +0000

    Update lib: 2.2.feature/IRPE-965-no-model-processing-option-4885

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,10 +1,10 @@
-import { BaseEntity } from '../entities/BaseEntity';
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { Coordinate } from '../common/Coordinate';
-import { BoundingBox } from '../common/BoundingBox';
 import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
+import { Coordinate } from '../common/Coordinate';
+import { BoundingBox } from '../common/BoundingBox';
+import { BaseEntity } from '../entities/BaseEntity';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 export declare class CaseEventObjects extends BaseEntity {
