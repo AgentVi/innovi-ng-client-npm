1c1f83a3be173fb0d905c222326288a7290f3704
Circle CI <circleci@agentvi.com>
2025-03-19 17:24:18 +0000

    Update lib: 2.2.feature/inv-8622-mcc-add-ad-search-to-user-invite-for-merge-4891

--- lib/common/RuleSpec.d.ts
+++ lib/common/RuleSpec.d.ts
@@ -1,11 +1,11 @@
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { RuleDefault } from '../common/RuleDefault';
 import { ObjectTypeNode } from '../common/ObjectTypeNode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { RuleAutoSensitivityCode } from '../enums/RuleAutoSensitivityCode';
 import { RuleDefaultSensitivity } from '../common/RuleDefaultSensitivity';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 export declare class RuleSpec {
     behaviorType: BehaviorTypeCode;
     ruleTypeName: string;
@@ -15,6 +15,7 @@ export declare class RuleSpec {
     minSpeed: RuleDefault;
     peopleInGroup: RuleDefault;
     clusterDistance: RuleDefault;
+    intervalTime: RuleDefault;
     referenceCrop: boolean;
     objectHierarchy: ObjectTypeNode[];
     sensorTypes: SensorTypeCode[];
@@ -24,5 +25,5 @@ export declare class RuleSpec {
     autoSensitivity: RuleAutoSensitivityCode[];
     sensitivity: RuleDefaultSensitivity;
     existsInWatchlist: boolean[];
-    constructor(behaviorType?: BehaviorTypeCode, ruleTypeName?: string, objectTypes?: ObjectTypeCode[], isLineDrawing?: boolean, dwellTime?: RuleDefault, minSpeed?: RuleDefault, peopleInGroup?: RuleDefault, clusterDistance?: RuleDefault, referenceCrop?: boolean, objectHierarchy?: ObjectTypeNode[], sensorTypes?: SensorTypeCode[], externalModel?: string, viewTypes?: ViewTypeCode[], excludeObjectTypes?: ObjectTypeCode[], autoSensitivity?: RuleAutoSensitivityCode[], sensitivity?: RuleDefaultSensitivity, existsInWatchlist?: boolean[]);
+    constructor(behaviorType?: BehaviorTypeCode, ruleTypeName?: string, objectTypes?: ObjectTypeCode[], isLineDrawing?: boolean, dwellTime?: RuleDefault, minSpeed?: RuleDefault, peopleInGroup?: RuleDefault, clusterDistance?: RuleDefault, intervalTime?: RuleDefault, referenceCrop?: boolean, objectHierarchy?: ObjectTypeNode[], sensorTypes?: SensorTypeCode[], externalModel?: string, viewTypes?: ViewTypeCode[], excludeObjectTypes?: ObjectTypeCode[], autoSensitivity?: RuleAutoSensitivityCode[], sensitivity?: RuleDefaultSensitivity, existsInWatchlist?: boolean[]);
 }
--- lib/entities/Rule.d.ts
+++ lib/entities/Rule.d.ts
@@ -1,10 +1,10 @@
+import { Sensitivity } from '../common/Sensitivity';
 import { RulePolygon } from '../common/RulePolygon';
 import { RuleAutoSensitivityCode } from '../enums/RuleAutoSensitivityCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { Sensitivity } from '../common/Sensitivity';
 export declare class Rule extends BaseEntity {
     accountId: string;
     sensorId: string;
@@ -22,6 +22,7 @@ export declare class Rule extends BaseEntity {
     minSpeed: number;
     maxObjectsInGroup: number;
     clusterDistance: number;
+    intervalTime: number;
     polygons: RulePolygon[];
     referenceCrop: string;
     createImage: boolean;
