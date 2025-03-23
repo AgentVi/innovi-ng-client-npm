cc2292c99405304b87fd55109aea8fba9f99f843
Circle CI <circleci@agentvi.com>
2025-03-23 12:42:06 +0000

    Update lib: 2.2.feature/inv-8622-mcc-add-ad-search-to-user-invite-for-merge-4898

--- lib/messages/EntitiesResponseOfAccountStatSummary.d.ts
+++ lib/messages/EntitiesResponseOfAccountStatSummary.d.ts
@@ -1,5 +1,5 @@
-import { AccountStatSummary } from '../common/AccountStatSummary';
 import { EntitiesResponse } from '../messages/EntitiesResponse';
+import { AccountStatSummary } from '../common/AccountStatSummary';
 export declare class EntitiesResponseOfAccountStatSummary extends EntitiesResponse {
     list: AccountStatSummary[];
     code: number;
