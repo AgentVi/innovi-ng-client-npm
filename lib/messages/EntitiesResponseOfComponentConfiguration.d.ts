e67f99b28bf36652a0c3dc3167a4646547702d5d
Circle CI <circleci@agentvi.com>
2025-03-25 18:42:30 +0000

    Update lib: 2.2.fix/IRPE-997-crops-timeout-hotfix-4914

--- lib/messages/EntitiesResponseOfComponentConfiguration.d.ts
+++ lib/messages/EntitiesResponseOfComponentConfiguration.d.ts
@@ -1,5 +1,5 @@
-import { ComponentConfiguration } from '../entities/ComponentConfiguration';
 import { EntitiesResponse } from '../messages/EntitiesResponse';
+import { ComponentConfiguration } from '../entities/ComponentConfiguration';
 export declare class EntitiesResponseOfComponentConfiguration extends EntitiesResponse {
     list: ComponentConfiguration[];
     code: number;
