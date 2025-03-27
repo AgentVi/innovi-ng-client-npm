864645ed325e791ca14925764a430055eb1206b0
Circle CI <circleci@agentvi.com>
2025-03-27 14:37:56 +0000

    Update lib: 2.2.fix/IRPE-516-improve-elastic-err-logging-hotfix-2025.2-4926

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
-import { TimestampedCrop } from '../common/TimestampedCrop';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { TimestampedCrop } from '../common/TimestampedCrop';
+import { BaseEntity } from '../entities/BaseEntity';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { Coordinate } from '../common/Coordinate';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
-import { BaseEntity } from '../entities/BaseEntity';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
