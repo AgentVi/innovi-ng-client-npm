323def55aa561b42dd966304c3b4f079de9293c2
Circle CI <circleci@agentvi.com>
2025-04-14 13:22:06 +0000

    Update lib: 2.2.feature/IRPE-989-object-metadata-from-agent-4982

--- lib/messages/EntitiesResponseOfConfiguration.d.ts
+++ lib/messages/EntitiesResponseOfConfiguration.d.ts
@@ -1,5 +1,5 @@
-import { EntitiesResponse } from '../messages/EntitiesResponse';
 import { ConfigurationVersion } from '../entities/ConfigurationVersion';
+import { EntitiesResponse } from '../messages/EntitiesResponse';
 export declare class EntitiesResponseOfConfiguration extends EntitiesResponse {
     list: ConfigurationVersion[];
     code: number;
