64de9b52f85003cde4e6e931a2a4ff28e867c5d2
Circle CI <circleci@agentvi.com>
2025-05-22 09:28:18 +0000

    Update lib: 2.2.feature/IRPE-989-object-metadata-from-agent-5145

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,13 +1,13 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
+import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { Coordinate } from '../common/Coordinate';
+import { Sensor } from '../entities/Sensor';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Preset } from '../entities/Preset';
 import { RulePolygon } from '../common/RulePolygon';
-import { Sensor } from '../entities/Sensor';
 import { SensorsGroup } from '../common/SensorsGroup';
 import * as i0 from "@angular/core";
 /**
