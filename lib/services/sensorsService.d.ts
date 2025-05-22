a1df390836c437b78ed699514c9382c95a0abae4
Circle CI <circleci@agentvi.com>
2025-05-22 13:45:18 +0000

    Update lib: 2.2.feature/IRPE-1127-redis-metrics-5153

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,7 +1,5 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
@@ -9,6 +7,8 @@ import { Preset } from '../entities/Preset';
 import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
 import { SensorsGroup } from '../common/SensorsGroup';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
