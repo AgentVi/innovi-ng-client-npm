d3031722459f2ca0aa08937323e371e191caa9a9
Circle CI <circleci@agentvi.com>
2025-05-28 11:23:01 +0000

    Update lib: 2.2.feature/irpe-1173-add-support-bundle-5167

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
+import { SensorDebugInfo } from '../common/SensorDebugInfo';
+import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { Thresholds } from '../common/Thresholds';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { Thresholds } from '../common/Thresholds';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
