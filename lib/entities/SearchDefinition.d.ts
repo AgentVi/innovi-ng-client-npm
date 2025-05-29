a29efef3530f44da3df58f75334c596cb912cc76
Circle CI <circleci@agentvi.com>
2025-05-29 10:54:19 +0000

    Update lib: 2.2.feature/irpe-779-heatmap-with-object-metadata-from-agent-5175

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { TimeFrame } from '../common/TimeFrame';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchObject } from '../common/SearchObject';
-import { ObjectData } from '../common/ObjectData';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { SearchBehavior } from '../common/SearchBehavior';
+import { ObjectData } from '../common/ObjectData';
+import { Coordinate } from '../common/Coordinate';
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchObject } from '../common/SearchObject';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
