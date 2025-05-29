853c524a401b26224dc05090985959037f15dde1
Circle CI <circleci@agentvi.com>
2025-05-29 13:25:29 +0000

    Update lib: 2.2.feature/IRPE-989-object-metadata-from-agent-5183

--- lib/messages/EntitiesResponseOfEvent.d.ts
+++ lib/messages/EntitiesResponseOfEvent.d.ts
@@ -1,5 +1,5 @@
-import { Event } from '../entities/Event';
 import { EntitiesResponse } from '../messages/EntitiesResponse';
+import { Event } from '../entities/Event';
 export declare class EntitiesResponseOfEvent extends EntitiesResponse {
     list: Event[];
     code: number;
