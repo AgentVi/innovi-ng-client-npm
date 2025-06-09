cb10821ba3c6450248f5d0bf578761b9b6c0281b
Circle CI <circleci@agentvi.com>
2025-06-09 12:08:02 +0000

    Update lib: 2.2.feature/IRPE-1284-metrics-improvements-5241

--- lib/common/ObjectInstance.d.ts
+++ lib/common/ObjectInstance.d.ts
@@ -1,10 +1,10 @@
+import { ObjectColor } from '../common/ObjectColor';
+import { Dimension } from '../common/Dimension';
 import { BoundingBox } from '../common/BoundingBox';
 import { Point } from '../common/Point';
 import { Coordinate } from '../common/Coordinate';
 import { ExternalModelResults } from '../common/ExternalModelResults';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { ObjectColor } from '../common/ObjectColor';
-import { Dimension } from '../common/Dimension';
 export declare class ObjectInstance {
     timestamp: number;
     objectType: ObjectTypeCode;
