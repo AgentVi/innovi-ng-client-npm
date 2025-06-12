b90618f436558cb0db7b9f2290ac28972485e0c4
Circle CI <circleci@agentvi.com>
2025-06-12 16:28:22 +0000

    Update lib: 2.2.feature/IRPE-1303-add-logging-in-acs-5263

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
-import { BaseEntity } from '../entities/BaseEntity';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Thresholds } from '../common/Thresholds';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
--- lib/messages/EntityResponseOfSensorStatus.d.ts
+++ lib/messages/EntityResponseOfSensorStatus.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { SensorStatus } from '../entities/SensorStatus';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfSensorStatus extends EntityResponse {
     entity: SensorStatus;
     code: number;
