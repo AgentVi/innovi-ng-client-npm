265152c3fef6e58ac1d93e764e099bf6bdadad6e
Circle CI <circleci@agentvi.com>
2025-06-15 08:33:07 +0000

    Update lib: 2.2.5266

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,15 +1,15 @@
+import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { Thresholds } from '../common/Thresholds';
-import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StringKeyValue } from '../common/StringKeyValue';
+import { Thresholds } from '../common/Thresholds';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class Sensor extends BaseEntity {
     accountId: string;
     folderId: string;
@@ -50,7 +50,6 @@ export declare class Sensor extends BaseEntity {
     personRecognition: boolean;
     anonymizationType: SensorAnonymizationTypeCode;
     trackerImprovementInterval: number;
-    advancedSearch: boolean;
     id: string;
     _type: string;
     createdOn: number;
