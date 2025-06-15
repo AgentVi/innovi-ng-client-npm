f24c376903e1f110d51a1ab4638dded5a158abda
Circle CI <circleci@agentvi.com>
2025-06-15 07:16:39 +0000

    Update lib: 2.2.feature/IRPE-989-object-metadata-from-agent-5265

--- lib/services/searchService.d.ts
+++ lib/services/searchService.d.ts
@@ -1,9 +1,9 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { TimeUnitCode } from '../enums/TimeUnitCode';
-import { SearchDefinition } from '../entities/SearchDefinition';
 import { SearchEventFindRequestBody } from '../messages/SearchEventFindRequestBody';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { TimeUnitCode } from '../enums/TimeUnitCode';
+import { SearchDefinition } from '../entities/SearchDefinition';
 import * as i0 from "@angular/core";
 /**
  * Services for metadata search actions
