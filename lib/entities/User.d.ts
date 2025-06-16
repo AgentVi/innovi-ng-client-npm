600da094635dcb1f307456cf9dd7760de842612e
Circle CI <circleci@agentvi.com>
2025-06-16 13:25:20 +0000

    Update lib: 2.2.feature/codex-resolve-merge-conflicts-and-ensure-security-checks-5286

--- lib/entities/User.d.ts
+++ lib/entities/User.d.ts
@@ -1,7 +1,7 @@
-import { UserTypeCode } from '../enums/UserTypeCode';
 import { UserStatusCode } from '../enums/UserStatusCode';
 import { OriginTypeCode } from '../enums/OriginTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
+import { UserTypeCode } from '../enums/UserTypeCode';
 export declare class User extends BaseEntity {
     name: string;
     email: string;
