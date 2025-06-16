29beb4bbe747c61456097956407b254aeabed22a
Circle CI <circleci@agentvi.com>
2025-06-16 12:15:35 +0000

    Update lib: 2.2.feature/codex-add-permissions-validation-to-addsensormodel-and-deletesenso-5273

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -4,11 +4,11 @@ import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Preset } from '../entities/Preset';
-import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
+import { SensorsGroup } from '../common/SensorsGroup';
+import { RulePolygon } from '../common/RulePolygon';
 import { Coordinate } from '../common/Coordinate';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
-import { SensorsGroup } from '../common/SensorsGroup';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
