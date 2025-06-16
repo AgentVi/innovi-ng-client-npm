035d2cf58348bfd3b0bdac7e9d1e0550b4d0aa4f
Circle CI <circleci@agentvi.com>
2025-06-16 10:49:43 +0000

    Update lib: 2.2.5271

--- lib/messages/EntityResponseOfTimeline.d.ts
+++ lib/messages/EntityResponseOfTimeline.d.ts
@@ -1,5 +1,5 @@
-import { EntityResponse } from '../messages/EntityResponse';
 import { Timeline } from '../common/Timeline';
+import { EntityResponse } from '../messages/EntityResponse';
 export declare class EntityResponseOfTimeline extends EntityResponse {
     entity: Timeline;
     code: number;
