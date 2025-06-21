dd402fc4383745713fd0dee5ea38ea0181bda462
Circle CI <circleci@agentvi.com>
2025-06-21 15:18:08 +0000

    Update lib: 2.2.fix/ipe-1032-enforce-auth-sensors-5321

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchObject } from '../common/SearchObject';
+import { Coordinate } from '../common/Coordinate';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { Coordinate } from '../common/Coordinate';
-import { SearchBehavior } from '../common/SearchBehavior';
 import { BaseEntity } from '../entities/BaseEntity';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
