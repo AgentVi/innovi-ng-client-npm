25273d341f04acd012255aa049a4cdb78a2d7e64
Circle CI <circleci@agentvi.com>
2025-06-22 17:09:47 +0000

    Update lib: 2.2.feature/IRPE-1174-supprt-bundle-improvements-5327

--- lib/sockets/SocketHealthStatusNotification.d.ts
+++ lib/sockets/SocketHealthStatusNotification.d.ts
@@ -1,7 +1,7 @@
-import { SocketOpCode } from '../enums/SocketOpCode';
 import { SensorStatus } from '../entities/SensorStatus';
 import { AgentStatus } from '../entities/AgentStatus';
 import { ApplianceStatus } from '../entities/ApplianceStatus';
+import { SocketOpCode } from '../enums/SocketOpCode';
 export declare class SocketHealthStatusNotification {
     op: SocketOpCode;
     error: string;
