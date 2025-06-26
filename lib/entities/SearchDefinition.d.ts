1dc110c325c1f5934a6e38e5bf4b5ae97424ac16
Circle CI <circleci@agentvi.com>
2025-06-26 14:43:12 +0000

    Update lib: 2.2.fix/IRPE-1326-face-recognition-5366

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
-import { ObjectData } from '../common/ObjectData';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { TimeFrame } from '../common/TimeFrame';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchObject } from '../common/SearchObject';
+import { ObjectData } from '../common/ObjectData';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { Coordinate } from '../common/Coordinate';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { TimeFrame } from '../common/TimeFrame';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
