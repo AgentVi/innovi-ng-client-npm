486fd11a52e22a16423d2cd38d0052855fa5d76f
Circle CI <circleci@agentvi.com>
2025-06-30 10:48:29 +0000

    Update lib: 2.2.feature/IRPE-1174-supprt-bundle-improvements-5385

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
+import { TimeFrame } from '../common/TimeFrame';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { SearchObject } from '../common/SearchObject';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
-import { TimeFrame } from '../common/TimeFrame';
-import { SearchBehavior } from '../common/SearchBehavior';
-import { SearchObject } from '../common/SearchObject';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
