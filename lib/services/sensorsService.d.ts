cf3580db97d7a12c0d810af43bd4eddab713cf5f
Circle CI <circleci@agentvi.com>
2025-07-09 08:58:01 +0000

    Update lib: 2.2.fix/irpe-779-heatmap-handle-error-from-visualizer-5452

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { Coordinate } from '../common/Coordinate';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { Preset } from '../entities/Preset';
-import { SensorsGroup } from '../common/SensorsGroup';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { SensorsGroup } from '../common/SensorsGroup';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { Preset } from '../entities/Preset';
 import { RulePolygon } from '../common/RulePolygon';
 import { Sensor } from '../entities/Sensor';
 import * as i0 from "@angular/core";
