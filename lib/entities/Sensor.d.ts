33b17b85c9b5095be2151ecacc163af0b3f5c8c6
Circle CI <circleci@agentvi.com>
2025-07-15 13:53:36 +0000

    Update lib: 2.2.fix/IRPE-1382-PAI-attribute-5486

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,14 +1,14 @@
 import { BaseEntity } from '../entities/BaseEntity';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
+import { Coordinate } from '../common/Coordinate';
+import { Thresholds } from '../common/Thresholds';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
-import { Coordinate } from '../common/Coordinate';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { StringKeyValue } from '../common/StringKeyValue';
-import { Thresholds } from '../common/Thresholds';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 export declare class Sensor extends BaseEntity {
     accountId: string;
@@ -50,7 +50,6 @@ export declare class Sensor extends BaseEntity {
     personRecognition: boolean;
     anonymizationType: SensorAnonymizationTypeCode;
     trackerImprovementInterval: number;
-    advancedSearch: boolean;
     id: string;
     _type: string;
     createdOn: number;
