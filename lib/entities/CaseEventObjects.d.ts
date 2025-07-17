6e0c25731f0e935071042d93c3f82b61cfc661c0
Circle CI <circleci@agentvi.com>
2025-07-17 09:42:15 +0000

    Update lib: 2.2.fix/irpe-1052-insights-no-events-desplayed-in-last-2-hrs-for-uct-camera-new-5517

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
-import { Coordinate } from '../common/Coordinate';
-import { BoundingBox } from '../common/BoundingBox';
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
+import { EventStatusCode } from '../enums/EventStatusCode';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { BoundingBox } from '../common/BoundingBox';
 import { TimestampedCrop } from '../common/TimestampedCrop';
+import { BaseEntity } from '../entities/BaseEntity';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
