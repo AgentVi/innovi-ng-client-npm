261847fc76b2478d0243b20df7cbfe97c265cdd1
Circle CI <circleci@agentvi.com>
2025-07-20 12:37:16 +0000

    Update lib: 2.2.feature/IRPE-1451-branch-from-2025.R1.1-proxy-logging-5531

--- lib/services/searchService.d.ts
+++ lib/services/searchService.d.ts
@@ -143,11 +143,6 @@ export declare class SearchService {
      * @Return: QueryResponse<SearchEvent>
      */
     getSensorHeatmapEvents(sessionId?: string, sensorId?: string, row?: number, col?: number, page?: number, pageSize?: number): import("rxjs").Observable<any>;
-    /**
-     * Get single POI (Point of Interest) by sensorId and objectId
-     * @Return EntityResponse<Poi>
-     */
-    getPoi(sensorId?: string, objectId?: string): import("rxjs").Observable<any>;
     static ɵfac: i0.ɵɵFactoryDeclaration<SearchService, never>;
     static ɵprov: i0.ɵɵInjectableDeclaration<SearchService>;
 }
