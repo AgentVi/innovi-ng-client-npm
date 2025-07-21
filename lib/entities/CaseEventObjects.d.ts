daedcf96ceed3070626250d49103f320eb7893f2
Circle CI <circleci@agentvi.com>
2025-07-21 06:53:01 +0000

    Update lib: 2.2.feature/irpe-201-use-parametrized-statements-pg-queries-5542

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,11 +1,11 @@
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { Coordinate } from '../common/Coordinate';
-import { ObjectInfo } from '../common/ObjectInfo';
-import { BoundingBox } from '../common/BoundingBox';
+import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { TimestampedCrop } from '../common/TimestampedCrop';
+import { ObjectInfo } from '../common/ObjectInfo';
+import { BoundingBox } from '../common/BoundingBox';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { Coordinate } from '../common/Coordinate';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
