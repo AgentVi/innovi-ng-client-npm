7fbeed01327c55fba384656b06ce02b7eca79ec2
Circle CI <circleci@agentvi.com>
2025-07-22 15:29:25 +0000

    Update lib: 2.2.5562

--- lib/messages/QueryResponseOfIntegrationStatus.d.ts
+++ lib/messages/QueryResponseOfIntegrationStatus.d.ts
@@ -1,5 +1,5 @@
-import { IntegrationStatus } from '../entities/IntegrationStatus';
 import { QueryResponse } from '../messages/QueryResponse';
+import { IntegrationStatus } from '../entities/IntegrationStatus';
 export declare class QueryResponseOfIntegrationStatus extends QueryResponse {
     list: IntegrationStatus[];
     code: number;
