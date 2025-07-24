bf25dd0d31a27d5525c8cd4440ff6eb98ba70693
Circle CI <circleci@agentvi.com>
2025-07-24 09:01:21 +0000

    Update lib: 2.2.feature/IRPE-1468-go-toolchain-5569

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,10 +1,10 @@
-import { BaseEntity } from '../entities/BaseEntity';
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { TimeFrame } from '../common/TimeFrame';
-import { SearchObject } from '../common/SearchObject';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { SearchBehavior } from '../common/SearchBehavior';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 export declare class SearchDefinition extends BaseEntity {
