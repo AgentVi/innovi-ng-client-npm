ade62aa81b223ff7dfefbb33df28d9b4f76ca492
Circle CI <circleci@agentvi.com>
2025-07-27 16:58:59 +0000

    Update lib: 2.2.feature/lyalin-add-mcp-servers-5577

--- lib/sockets/SocketEventNotification.d.ts
+++ lib/sockets/SocketEventNotification.d.ts
@@ -1,5 +1,5 @@
-import { SocketOpCode } from '../enums/SocketOpCode';
 import { SocketEventNotificationPayload } from '../sockets/SocketEventNotificationPayload';
+import { SocketOpCode } from '../enums/SocketOpCode';
 export declare class SocketEventNotification {
     op: SocketOpCode;
     payload: SocketEventNotificationPayload;
