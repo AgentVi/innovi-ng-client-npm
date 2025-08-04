0e413e064ab396adb4a6a22bdfd3a5d20933b19d
Circle CI <circleci@agentvi.com>
2025-08-04 13:36:51 +0000

    Update lib: 2.2.feature/IRPE-1519-added-unit-tests-for-NearestTimeBBoxes-5609

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,11 +1,11 @@
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { TimestampedCrop } from '../common/TimestampedCrop';
-import { BaseEntity } from '../entities/BaseEntity';
-import { Coordinate } from '../common/Coordinate';
 import { ObjectInfo } from '../common/ObjectInfo';
+import { TimestampedCrop } from '../common/TimestampedCrop';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { EventStatusCode } from '../enums/EventStatusCode';
 import { BoundingBox } from '../common/BoundingBox';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
