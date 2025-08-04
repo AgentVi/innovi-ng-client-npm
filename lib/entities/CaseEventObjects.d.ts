bbb877c6351c0f0408d454875dd51b5af870311b
Circle CI <circleci@agentvi.com>
2025-08-04 05:03:57 +0000

    Update lib: 2.2.feature/IRPE-1506-model-bug-fixes-5605

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
-import { Coordinate } from '../common/Coordinate';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { EventStatusCode } from '../enums/EventStatusCode';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
-import { TimestampedCrop } from '../common/TimestampedCrop';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { BaseEntity } from '../entities/BaseEntity';
+import { EventStatusCode } from '../enums/EventStatusCode';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { TimestampedCrop } from '../common/TimestampedCrop';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
