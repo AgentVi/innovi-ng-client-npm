81363bc6e1a8a69e40d3415b349979bfc58d6233
Circle CI <circleci@agentvi.com>
2025-08-06 09:03:44 +0000

    Update lib: 2.2.feature/IRPE-1530-log-failed-upload-eventid-5629

--- lib/messages/EntitiesResponseOfPoi.d.ts
+++
@@ -1,7 +0,0 @@
-import { Poi } from '../entities/Poi';
-import { EntitiesResponse } from '../messages/EntitiesResponse';
-export declare class EntitiesResponseOfPoi extends EntitiesResponse {
-    entities: Poi[];
-    code: number;
-    error: string;
-}
