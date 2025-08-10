5feb303317dba2258428e4ae06abc8c8df9f7fc5
Circle CI <circleci@agentvi.com>
2025-08-10 06:39:52 +0000

    Update lib: 2.2.fix/irpe-1542-agent-proxy-errors-5672

--- lib/sockets/SocketHealthStatusNotification.d.ts
+++ lib/sockets/SocketHealthStatusNotification.d.ts
@@ -1,7 +1,7 @@
+import { SensorStatus } from '../entities/SensorStatus';
 import { AgentStatus } from '../entities/AgentStatus';
 import { ApplianceStatus } from '../entities/ApplianceStatus';
 import { SocketOpCode } from '../enums/SocketOpCode';
-import { SensorStatus } from '../entities/SensorStatus';
 export declare class SocketHealthStatusNotification {
     op: SocketOpCode;
     error: string;
