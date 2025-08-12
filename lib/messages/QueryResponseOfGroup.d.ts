bfa22d05d11900517c21a8c9cc4961371c39aa68
Circle CI <circleci@agentvi.com>
2025-08-12 12:59:41 +0000

    Update lib: 2.2.fix/split-newsearch-oldsearch-5720

--- lib/messages/QueryResponseOfGroup.d.ts
+++ lib/messages/QueryResponseOfGroup.d.ts
@@ -1,5 +1,5 @@
-import { QueryResponse } from '../messages/QueryResponse';
 import { Group } from '../entities/Group';
+import { QueryResponse } from '../messages/QueryResponse';
 export declare class QueryResponseOfGroup extends QueryResponse {
     list: Group[];
     code: number;
