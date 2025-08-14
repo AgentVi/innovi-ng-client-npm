80d2cee6a5bc66609e0dfa1a448eb7f2ad68197a
Circle CI <circleci@agentvi.com>
2025-08-14 14:49:15 +0000

    Update lib: 2.2.fix/irpe-1097-remove-sensative-hardcoded-data-from-repo-5765

--- lib/messages/EntityResponseOfHealthEvent.d.ts
+++ lib/messages/EntityResponseOfHealthEvent.d.ts
@@ -1,5 +1,5 @@
-import { HealthEvent } from '../entities/HealthEvent';
 import { EntityResponse } from '../messages/EntityResponse';
+import { HealthEvent } from '../entities/HealthEvent';
 export declare class EntityResponseOfHealthEvent extends EntityResponse {
     entity: HealthEvent;
     code: number;
