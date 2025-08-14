380d66b167441529e9735f95cbb2775fa642532e
Circle CI <circleci@agentvi.com>
2025-08-14 08:20:54 +0000

    Update lib: 2.2.feature/irpe-1557-compliance-related-ui-changes-for-master-5761

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
 import { Sensor } from '../entities/Sensor';
+import { SensorsGroup } from '../common/SensorsGroup';
 import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
-import { SensorsGroup } from '../common/SensorsGroup';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
