66ae6ab44766d9eca3ebcb796567b53fd14d2dd3
Circle CI <circleci@agentvi.com>
2025-08-24 12:03:08 +0000

    Update lib: 2.2.fix/irpe-1097-remove-sensative-hardcoded-data-from-repo-sendGridAPiKey-5833

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,10 +1,10 @@
 import { Coordinate } from '../common/Coordinate';
-import { SearchObject } from '../common/SearchObject';
-import { ObjectData } from '../common/ObjectData';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { SearchObject } from '../common/SearchObject';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchBehavior } from '../common/SearchBehavior';
+import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
