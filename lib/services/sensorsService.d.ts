c3c0bc29cf506250e9b01b065a5b17563997fdaf
Circle CI <circleci@agentvi.com>
2025-08-29 20:34:43 +0000

    Update lib: 2.2.feature/irpe-1298-refactor-license-manager-5854

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { SensorsGroup } from '../common/SensorsGroup';
+import { Sensor } from '../entities/Sensor';
 import { Coordinate } from '../common/Coordinate';
+import { Preset } from '../entities/Preset';
+import { SensorsGroup } from '../common/SensorsGroup';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { Preset } from '../entities/Preset';
-import { Sensor } from '../entities/Sensor';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
