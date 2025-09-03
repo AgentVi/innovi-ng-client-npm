c0fd5b4ee5b5fb5f962fdc4d9f8e3f24065f9eaa
Circle CI <circleci@agentvi.com>
2025-09-03 17:21:58 +0000

    Update lib: 2.2.fix/irpe-000-try-fix-zookeeper-version-issue-for-release-5883

--- lib/messages/EntityResponseOfPoi.d.ts
+++ lib/messages/EntityResponseOfPoi.d.ts
@@ -1,5 +1,5 @@
-import { Poi } from '../entities/Poi';
 import { EntityResponse } from '../messages/EntityResponse';
+import { Poi } from '../entities/Poi';
 export declare class EntityResponseOfPoi extends EntityResponse {
     entity: Poi;
     code: number;
