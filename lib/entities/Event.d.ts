0b08b52600820b58fe8a8e001180bc74cdb5439b
Circle CI <circleci@agentvi.com>
2025-09-07 13:44:25 +0000

    Update lib: 2.2.5899

--- lib/entities/Event.d.ts
+++ lib/entities/Event.d.ts
@@ -1,3 +1,4 @@
+import { BoundingBox } from '../common/BoundingBox';
 import { BaseEntity } from '../entities/BaseEntity';
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
@@ -5,7 +6,6 @@ import { EventStatusCode } from '../enums/EventStatusCode';
 import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
-import { BoundingBox } from '../common/BoundingBox';
 export declare class Event extends BaseEntity {
     sensorId: string;
     extSensorId: string;
