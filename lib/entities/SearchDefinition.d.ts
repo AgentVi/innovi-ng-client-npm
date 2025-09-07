771c20c6a7cc5e200c2912b2a4eeb0b233925a97
Circle CI <circleci@agentvi.com>
2025-09-07 13:48:33 +0000

    Update lib: 2.2.fix/irpe-1097-fix-token-api-secret-value-for-release-5901

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
+import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchBehavior } from '../common/SearchBehavior';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
-import { SearchObject } from '../common/SearchObject';
+import { SearchBehavior } from '../common/SearchBehavior';
 import { ObjectData } from '../common/ObjectData';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { SearchObject } from '../common/SearchObject';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
