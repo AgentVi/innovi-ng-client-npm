8a5778340f84a21b7a997c2a644f3c30a9274c37
Circle CI <circleci@agentvi.com>
2025-09-19 07:56:11 +0000

    Update lib: 2.2.fix/dos2unix-for-create-kratos-db-when-running-in-wsl-5953

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
-import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { Thresholds } from '../common/Thresholds';
-import { BaseEntity } from '../entities/BaseEntity';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
-import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { ViewTypeCode } from '../enums/ViewTypeCode';
 export declare class Sensor extends BaseEntity {
