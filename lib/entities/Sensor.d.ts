c2d7b7f2223bc3cca4a3c0f9c2e25cff4ce41209
Circle CI <circleci@agentvi.com>
2025-09-24 16:26:53 +0000

    Update lib: 2.2.fix/irpe-1028-enforce-auth-sensors-rules-cases-5971

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,11 +1,11 @@
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { Thresholds } from '../common/Thresholds';
