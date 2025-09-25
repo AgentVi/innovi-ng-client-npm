9a5b327a5665d2035c03420cd5e631f414c8d154
Circle CI <circleci@agentvi.com>
2025-09-25 07:31:29 +0000

    Update lib: 2.2.feature/irpe-1641-remove-ability-to-combine-licenses-5974

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,12 +1,12 @@
 import { EventStatusCode } from '../enums/EventStatusCode';
-import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { TimestampedCrop } from '../common/TimestampedCrop';
-import { ObjectTypeCode } from '../enums/ObjectTypeCode';
-import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BoundingBox } from '../common/BoundingBox';
+import { TimestampedCrop } from '../common/TimestampedCrop';
+import { ObjectTypeCode } from '../enums/ObjectTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { BaseEntity } from '../entities/BaseEntity';
+import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
 export declare class CaseEventObjects extends BaseEntity {
     sensorId: string;
     extSensorId: string;
