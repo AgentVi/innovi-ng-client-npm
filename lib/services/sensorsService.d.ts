a50c44682841d29dc2d976d6bc66978fa7ae0aec
Circle CI <circleci@agentvi.com>
2025-10-02 08:11:22 +0000

    Update lib: 2.2.feature/irpe-1641-remove-ability-to-combine-licenses-5999

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -3,12 +3,12 @@ import { CoreConfig } from '../../config';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
 import { RulePolygon } from '../common/RulePolygon';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { SensorsGroup } from '../common/SensorsGroup';
-import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { Sensor } from '../entities/Sensor';
+import { Coordinate } from '../common/Coordinate';
 import * as i0 from "@angular/core";
 /**
  * Services for sensor actions
