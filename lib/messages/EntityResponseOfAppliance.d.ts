85bb7b2e0cddd74d0d9cac9e9bf55cf324a845e6
Circle CI <circleci@agentvi.com>
2025-10-16 04:26:08 +0000

    Update lib: 2.2.feature/IRPE-1636-generate-new-license-format-6032

--- lib/messages/EntityResponseOfAppliance.d.ts
+++ lib/messages/EntityResponseOfAppliance.d.ts
@@ -1,5 +1,5 @@
-import { Appliance } from '../entities/Appliance';
 import { EntityResponse } from '../messages/EntityResponse';
+import { Appliance } from '../entities/Appliance';
 export declare class EntityResponseOfAppliance extends EntityResponse {
     entity: Appliance;
     code: number;
