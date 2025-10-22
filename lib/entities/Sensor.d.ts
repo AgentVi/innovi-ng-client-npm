90ab20bf5730d4663c8b23224950803f69e1c315
Circle CI <circleci@agentvi.com>
2025-10-22 12:03:05 +0000

    Update lib: 2.2.feature/IRPE-1636-generate-new-license-format-6054

--- lib/entities/Sensor.d.ts
+++ lib/entities/Sensor.d.ts
@@ -1,13 +1,13 @@
-import { Coordinate } from '../common/Coordinate';
-import { Thresholds } from '../common/Thresholds';
+import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { StringKeyValue } from '../common/StringKeyValue';
+import { Thresholds } from '../common/Thresholds';
+import { ViewTypeCode } from '../enums/ViewTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { SensorResolutionCode } from '../enums/SensorResolutionCode';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { StringKeyValue } from '../common/StringKeyValue';
 import { SensorDebugInfo } from '../common/SensorDebugInfo';
-import { ViewTypeCode } from '../enums/ViewTypeCode';
-import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { SensorAnonymizationTypeCode } from '../enums/SensorAnonymizationTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class Sensor extends BaseEntity {
--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
 import { SensorsGroup } from '../common/SensorsGroup';
-import { FovGeoAttributes } from '../common/FovGeoAttributes';
+import { Preset } from '../entities/Preset';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { RulePolygon } from '../common/RulePolygon';
-import { Preset } from '../entities/Preset';
 import { Sensor } from '../entities/Sensor';
 import { Coordinate } from '../common/Coordinate';
+import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import * as i0 from "@angular/core";
