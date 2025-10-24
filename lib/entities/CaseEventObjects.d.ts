3ddb73a7b5108aa3d8155967c8a766b9463f1e70
Circle CI <circleci@agentvi.com>
2025-10-24 08:17:11 +0000

    Update lib: 2.2.release/2025.5.2-6062

--- lib/entities/CaseEventObjects.d.ts
+++ lib/entities/CaseEventObjects.d.ts
@@ -1,9 +1,9 @@
 import { ObjectTypeCode } from '../enums/ObjectTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
+import { Coordinate } from '../common/Coordinate';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { ObjectInfo } from '../common/ObjectInfo';
 import { BehaviorTypeCode } from '../enums/BehaviorTypeCode';
-import { Coordinate } from '../common/Coordinate';
 import { BoundingBox } from '../common/BoundingBox';
 import { TimestampedCrop } from '../common/TimestampedCrop';
 import { BaseEntity } from '../entities/BaseEntity';
--- lib/messages/AppliancesServiceFindSensorsRequest.d.ts
+++ lib/messages/AppliancesServiceFindSensorsRequest.d.ts
@@ -1,6 +1,6 @@
+import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 export declare class AppliancesServiceFindSensorsRequest {
     id: string;
     search: string;
