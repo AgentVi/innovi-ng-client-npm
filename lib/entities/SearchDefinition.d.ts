cbafb4a2e12a286237212b73170574e1fe4ed9ec
Circle CI <circleci@agentvi.com>
2025-10-27 18:37:07 +0000

    Update lib: 2.2.feature/IRPE-1774-remove-anim-from-cases-6077

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,12 +1,12 @@
-import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 import { TimeFrame } from '../common/TimeFrame';
+import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
+import { SearchBehavior } from '../common/SearchBehavior';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchBehavior } from '../common/SearchBehavior';
-import { SearchObject } from '../common/SearchObject';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
     userId: string;
