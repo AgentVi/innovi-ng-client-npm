d84ad35e4c2fd2c404e7a1767765f393217294b8
Circle CI <circleci@agentvi.com>
2025-10-29 14:47:01 +0000

    Update lib: 2.2.feature/IRPE-1756-collect-usage-information-for-api-keys-6092

--- lib/sockets/SocketEventNotification.d.ts
+++ lib/sockets/SocketEventNotification.d.ts
@@ -1,5 +1,5 @@
-import { SocketEventNotificationPayload } from '../sockets/SocketEventNotificationPayload';
 import { SocketOpCode } from '../enums/SocketOpCode';
+import { SocketEventNotificationPayload } from '../sockets/SocketEventNotificationPayload';
 export declare class SocketEventNotification {
     op: SocketOpCode;
     payload: SocketEventNotificationPayload;
