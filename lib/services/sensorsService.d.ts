c4bff73102302f46489d6f19de227d4a32f567a3
Circle CI <circleci@agentvi.com>
2025-10-29 10:02:45 +0000

    Update lib: 2.2.release/2025.5.2-6088

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,5 +1,6 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
+import { Sensor } from '../entities/Sensor';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
 import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
@@ -7,7 +8,6 @@ import { Preset } from '../entities/Preset';
 import { SensorsGroup } from '../common/SensorsGroup';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
-import { Sensor } from '../entities/Sensor';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
 /**
