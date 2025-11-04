69ff1ed90cda86c6ae4176f409d2145bc2777473
Circle CI <circleci@agentvi.com>
2025-11-04 08:14:55 +0000

    Update lib: 2.2.fix/irpe-1825-pai-limit-reached-6112

--- lib/messages/EntitiesResponseOfTimestampedCrop.d.ts
+++ lib/messages/EntitiesResponseOfTimestampedCrop.d.ts
@@ -1,5 +1,5 @@
-import { TimestampedCrop } from '../common/TimestampedCrop';
 import { EntitiesResponse } from '../messages/EntitiesResponse';
+import { TimestampedCrop } from '../common/TimestampedCrop';
 export declare class EntitiesResponseOfTimestampedCrop extends EntitiesResponse {
     list: TimestampedCrop[];
     code: number;
