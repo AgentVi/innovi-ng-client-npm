66c52425cfbaecbb280275a37df5e3c0d9a70e77
Circle CI <circleci@agentvi.com>
2025-11-10 11:54:23 +0000

    Update lib: 2.2.feature/IRPE-1636-generate-new-license-format-6132

--- lib/messages/EntityResponseOfPoiUnrecognized.d.ts
+++ lib/messages/EntityResponseOfPoiUnrecognized.d.ts
@@ -1,5 +1,5 @@
-import { PoiUnrecognized } from '../entities/PoiUnrecognized';
 import { EntityResponse } from '../messages/EntityResponse';
+import { PoiUnrecognized } from '../entities/PoiUnrecognized';
 export declare class EntityResponseOfPoiUnrecognized extends EntityResponse {
     entity: PoiUnrecognized;
     code: number;
