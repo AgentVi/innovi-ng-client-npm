818f30fe2b15504d47987a70e62592320fbb1671
Circle CI <circleci@agentvi.com>
2025-11-10 10:59:37 +0000

    Update lib: 2.2.feature/IRPE-1636-generate-new-license-format-6131

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,12 +1,12 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Sensor } from '../entities/Sensor';
+import { SensorsGroup } from '../common/SensorsGroup';
 import { Coordinate } from '../common/Coordinate';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
-import { SensorsGroup } from '../common/SensorsGroup';
-import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
