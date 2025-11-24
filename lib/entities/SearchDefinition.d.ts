9c93817b2dd70ce57fd9a5c656a99e2ecd3ef272
Circle CI <circleci@agentvi.com>
2025-11-24 14:40:11 +0000

    Update lib: 2.2.fix/gateway-service-not-reading-token-api-secret-6160

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,11 +1,11 @@
-import { Coordinate } from '../common/Coordinate';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
-import { LevelTypeCode } from '../enums/LevelTypeCode';
-import { BaseEntity } from '../entities/BaseEntity';
-import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { TimeFrame } from '../common/TimeFrame';
 import { SearchBehavior } from '../common/SearchBehavior';
 import { SearchObject } from '../common/SearchObject';
+import { LevelTypeCode } from '../enums/LevelTypeCode';
+import { Coordinate } from '../common/Coordinate';
+import { SearchScopeCode } from '../enums/SearchScopeCode';
+import { BaseEntity } from '../entities/BaseEntity';
+import { TimeFrame } from '../common/TimeFrame';
 import { ObjectData } from '../common/ObjectData';
 export declare class SearchDefinition extends BaseEntity {
     accountId: string;
