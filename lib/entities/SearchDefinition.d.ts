15a2e71f81d3a06f45a351cb12f6e53ba4c775f0
Circle CI <circleci@agentvi.com>
2025-12-05 16:26:13 +0000

    Update lib: 2.2.feature/IRPE-2025-add-wire-echo-to-event-service-6185

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,10 +1,10 @@
 import { Coordinate } from '../common/Coordinate';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
-import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { SearchBehavior } from '../common/SearchBehavior';
-import { TimeFrame } from '../common/TimeFrame';
 import { SearchObject } from '../common/SearchObject';
 import { ObjectData } from '../common/ObjectData';
+import { SearchTimeCode } from '../enums/SearchTimeCode';
+import { TimeFrame } from '../common/TimeFrame';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
