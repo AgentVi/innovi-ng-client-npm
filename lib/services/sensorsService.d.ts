1f801e72570a9173e7f6a7bab30d8e0a089eb2c4
Circle CI <circleci@agentvi.com>
2025-12-05 09:15:34 +0000

    Update lib: 2.2.feature/IRPE-2025-add-wire-echo-to-event-service-6178

--- lib/services/sensorsService.d.ts
+++ lib/services/sensorsService.d.ts
@@ -1,13 +1,13 @@
 import { RestUtil } from '../../utils/rest-util';
 import { CoreConfig } from '../../config';
-import { Sensor } from '../entities/Sensor';
-import { Coordinate } from '../common/Coordinate';
-import { SensorStatusCode } from '../enums/SensorStatusCode';
 import { StreamTypeCode } from '../enums/StreamTypeCode';
 import { Preset } from '../entities/Preset';
 import { SensorsGroup } from '../common/SensorsGroup';
 import { FovGeoAttributes } from '../common/FovGeoAttributes';
 import { SensorTypeCode } from '../enums/SensorTypeCode';
+import { SensorStatusCode } from '../enums/SensorStatusCode';
+import { Sensor } from '../entities/Sensor';
+import { Coordinate } from '../common/Coordinate';
 import { RulePolygon } from '../common/RulePolygon';
 import * as i0 from "@angular/core";
 /**
