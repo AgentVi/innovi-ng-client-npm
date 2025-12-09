e8cd4135e738f097b3420fc3ab1c898ef4b5d4e6
Circle CI <circleci@agentvi.com>
2025-12-09 15:36:29 +0000

    Update lib: 2.2.fix/irpe-1463-template-variables-6208

--- lib/entities/SearchDefinition.d.ts
+++ lib/entities/SearchDefinition.d.ts
@@ -1,10 +1,10 @@
+import { ObjectData } from '../common/ObjectData';
 import { SearchScopeCode } from '../enums/SearchScopeCode';
 import { SearchTimeCode } from '../enums/SearchTimeCode';
 import { TimeFrame } from '../common/TimeFrame';
 import { SearchObject } from '../common/SearchObject';
 import { Coordinate } from '../common/Coordinate';
 import { SearchBehavior } from '../common/SearchBehavior';
-import { ObjectData } from '../common/ObjectData';
 import { LevelTypeCode } from '../enums/LevelTypeCode';
 import { BaseEntity } from '../entities/BaseEntity';
 export declare class SearchDefinition extends BaseEntity {
